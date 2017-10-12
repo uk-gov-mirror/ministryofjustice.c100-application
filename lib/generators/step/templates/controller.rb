b7ae437f908116ac36e124cfba39d7388b6ca9c2
David Salgado <david.salgado@digital.justice.gov.uk>
2017-10-12 12:05:19 +0100

    Add more files we should be able to reuse
    NB: The generators have many occurences of 'tribunal_case' and
    TribunalCase - these will need to be replaced once we have the
    name of our core database model.

---
+++ lib/generators/step/templates/controller.rb
@@ -0,0 +1,14 @@
+module Steps::<%= task_name.camelize %>
+  class <%= step_name.camelize %>Controller < Steps::<%= task_name.camelize %>StepController
+    def edit
+      @form_object = <%= step_name.camelize %>Form.new(
+        tribunal_case: current_tribunal_case,
+        <%= step_name.underscore  %>: current_tribunal_case.<%= step_name.underscore %>
+      )
+    end
+
+    def update
+      update_and_advance(<%= step_name.camelize %>Form)
+    end
+  end
+end
---
+++ lib/generators/step/templates/form.rb
@@ -0,0 +1,36 @@
+module Steps::<%= task_name.camelize %>
+  class <%= step_name.camelize %>Form < BaseForm
+    # TODO: Add more attributes or change type if necessary
+    attribute :<%= step_name.underscore %>, String
+
+    # TODO: Delete this method and add different validation if you don't have a value object
+    def self.choices
+      <%= step_name.camelize %>.values.map(&:to_s)
+    end
+    validates_inclusion_of :<%= step_name.underscore %>, in: choices
+
+    private
+
+    # TODO: Delete this method if you don't have a value object
+    def <%= step_name.underscore %>_value
+      <%= step_name.camelize %>.new(<%= step_name.underscore %>)
+    end
+
+    # TODO: Change this method if you don't have a single value object
+    def changed?
+      tribunal_case.<%= step_name.underscore %> != <%= step_name.underscore %>_value
+    end
+
+    def persist!
+      raise 'No TribunalCase given' unless tribunal_case
+      return true unless changed?
+
+      # TODO: Update this to persist your form object if you don't have a single value object
+      tribunal_case.update(
+        <%= step_name.underscore %>: <%= step_name.underscore %>_value
+        # The following are dependent attributes that need to be reset
+        # TODO: Are there any dependent attributes? Reset them here.
+      )
+    end
+  end
+end
