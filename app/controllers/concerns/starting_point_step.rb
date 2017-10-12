1ef570a9c6c64de85e8ba13af555d02149237fe8
David Salgado <david.salgado@digital.justice.gov.uk>
2017-10-12 16:55:03 +0100

    Remove reference to 'intent' from starting step
    Intent was specific to the tax tribunals 
    application. We probably won't need it here.

--- app/controllers/concerns/starting_point_step.rb
+++ app/controllers/concerns/starting_point_step.rb
@@ -8,9 +8,9 @@ module StartingPointStep
   private
 
   def current_c100_application
-    # Only the step including this concern should create a tribunal case
+    # Only the step including this concern should create a C100 application
     # if there isn't one in the session - because it's the first
-    super || initialize_c100_application(intent: intent)
+    super || initialize_c100_application
   end
 
   def update_navigation_stack
