cda930bb45d75c4828e18e307b847316691d4a77
David Salgado <david.salgado@digital.justice.gov.uk>
2017-10-12 16:55:44 +0100

    Add CaseTypeForm

---
+++ app/forms/steps/nature_of_application/case_type_form.rb
@@ -0,0 +1,38 @@
+module Steps::NatureOfApplication
+  class CaseTypeForm < BaseForm
+    attribute :case_type, String
+
+    def self.choices
+      [
+        CaseType::CHILD_ARRANGEMENTS,
+        CaseType::PROHIBITED_STEPS,
+        CaseType::SPECIFIC_ISSUE
+      ].map(&:to_s)
+    end
+
+    validates_inclusion_of :case_type, in: choices
+
+    private
+
+    def case_type_value?
+      case_type
+    end
+
+    def case_type_value
+      CaseType.find_constant(case_type)
+    end
+
+    def changed?
+      c100_application.case_type != case_type_value
+    end
+
+    def persist!
+      raise 'No C100Application given' unless c100_application
+      return true unless case_type_value? && changed?
+
+      c100_application.update(
+        case_type: case_type_value
+      )
+    end
+  end
+end
