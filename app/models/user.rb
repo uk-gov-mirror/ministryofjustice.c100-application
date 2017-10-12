0558a69fd3ad00c18452688105a4e4444ec9edbf
David Salgado <david.salgado@digital.justice.gov.uk>
2017-10-12 14:11:37 +0100

    Remove model relationship preventing rails boot

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :registerable, :validatable, :trackable
 
   has_many :tribunal_cases, dependent: :destroy
-  has_many :pending_tribunal_cases, -> { not_submitted }, class_name: TribunalCase
 
   attribute :email, NormalisedEmailType.new
 
