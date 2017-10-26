8a45bb53d5bb8ea749c71a3d5b34190a785c0c24
David Salgado <david.salgado@digital.justice.gov.uk>
2017-10-26 10:32:07 +0100

    Add a script to set required env vars on heroku

---
+++ set_heroku_env_vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+heroku config:set \
+  EXTERNAL_URL=https://c100-demo.herokuapp.com \
+  RACK_ENV=production \
+  RAILS_ENV=production \
+  RAILS_LOG_TO_STDOUT=enabled \
+  RAILS_MAX_THREADS=3 \
+  RAILS_SERVE_STATIC_FILES=enabled \
+  SECRET_KEY_BASE=$(bundle exec rails secret) \
+  GOVUK_NOTIFY_API_KEY=DUMMYVALUE \
+  LANG=en_US.UTF-8
