a57d63c2a610c1100775abd4717616c39faa4fab
Jesus Laiz <zheileman@users.noreply.github.com>
2017-11-06 09:22:10 +0000

    Add DOB field to Applicants db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101130429) do
+ActiveRecord::Schema.define(version: 20171103090618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171101130429) do
     t.string   "mobile_phone"
     t.string   "email"
     t.uuid     "c100_application_id"
+    t.date     "dob"
     t.index ["c100_application_id"], name: "index_applicants_on_c100_application_id", using: :btree
   end
 
