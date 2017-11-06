b2c3a1cc032ed216e0a7427e7b3b702ab50150d5
Jesus Laiz <zheileman@users.noreply.github.com>
2017-11-06 09:27:07 +0000

    Make BaseForm behave more like AR/ActiveModel
    In order to have the ability of calling callbacks, which is needed by
    the `gov_uk_date_fields` gem.

--- spec/forms/base_form_spec.rb
+++ spec/forms/base_form_spec.rb
@@ -6,4 +6,39 @@ RSpec.describe BaseForm do
       expect(subject.persisted?).to eq(false)
     end
   end
+
+  describe '#new_record?' do
+    it 'always returns true' do
+      expect(subject.new_record?).to eq(true)
+    end
+  end
+
+  describe '#to_key' do
+    it 'always returns nil' do
+      expect(subject.to_key).to be_nil
+    end
+  end
+
+  describe '[]' do
+    let(:c100_application) { instance_double(C100Application) }
+
+    before do
+      subject.c100_application = c100_application
+    end
+
+    it 'read the attribute directly without using the method' do
+      expect(subject).not_to receive(:c100_application)
+      expect(subject[:c100_application]).to eq(c100_application)
+    end
+  end
+
+  describe '[]=' do
+    let(:c100_application) { instance_double(C100Application) }
+
+    it 'assigns the attribute directly without using the method' do
+      expect(subject).not_to receive(:c100_application=)
+      subject[:c100_application] = c100_application
+      expect(subject.c100_application).to eq(c100_application)
+    end
+  end
 end
