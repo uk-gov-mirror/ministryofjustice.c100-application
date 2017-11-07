612e86f4a70127876a430bb00563fe7cca83592a
Jesus Laiz <zheileman@users.noreply.github.com>
2017-11-07 09:28:47 +0000

    DRY crud controllers
    As we now have 2 entities (applicants and respondents) that requires CRUD
    actions in the controller, we can create a concern to be included in these
    and other future controllers to enforce the same interface.
    
    For now, I'm leaving untouched the list of attributes although many are
    common. Once we have more entities (children? etc.) we will have a more
    clear opinion of how many of these attributes can be shared and extracted
    to a superclass or a module.
    
    As part of another PR we can also DRY the form objects unless it makes
    them less readable/maintanable.

--- app/controllers/steps/applicant/personal_details_controller.rb
+++ app/controllers/steps/applicant/personal_details_controller.rb
@@ -1,47 +1,38 @@
 module Steps
   module Applicant
     class PersonalDetailsController < Steps::ApplicantStepController
-      before_action :set_existing_applicants
+      include CrudStep
 
       def edit
         @form_object = PersonalDetailsForm.new(
           c100_application: current_c100_application,
-          record_id: current_applicant.id,
-          full_name: current_applicant.full_name,
-          has_previous_name: current_applicant.has_previous_name,
-          previous_full_name: current_applicant.previous_full_name,
-          gender: current_applicant.gender,
-          dob: current_applicant.dob,
-          birthplace: current_applicant.birthplace,
-          address: current_applicant.address,
-          postcode: current_applicant.postcode,
-          home_phone: current_applicant.home_phone,
-          mobile_phone: current_applicant.mobile_phone,
-          email: current_applicant.email
+          record_id: current_record.id,
+          full_name: current_record.full_name,
+          has_previous_name: current_record.has_previous_name,
+          previous_full_name: current_record.previous_full_name,
+          gender: current_record.gender,
+          dob: current_record.dob,
+          birthplace: current_record.birthplace,
+          address: current_record.address,
+          postcode: current_record.postcode,
+          home_phone: current_record.home_phone,
+          mobile_phone: current_record.mobile_phone,
+          email: current_record.email
         )
       end
 
       def update
         update_and_advance(
           PersonalDetailsForm,
-          record_id: current_applicant.id,
+          record_id: current_record.id,
           as: params.fetch(:button, :applicants_finished)
         )
       end
 
-      def destroy
-        current_applicant.destroy
-        redirect_to action: :edit, id: current_c100_application.saved_applicants.first
-      end
-
       private
 
-      def current_applicant
-        @_current_applicant ||= current_c100_application.applicants.find_or_initialize_by(id: params[:id])
-      end
-
-      def set_existing_applicants
-        @existing_applicants = current_c100_application.saved_applicants
+      def record_collection
+        @_record_collection ||= current_c100_application.applicants
       end
     end
   end
