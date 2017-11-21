7306a7e7d9ebd35c1330aa61d4f58c2d0c72e6a4
Jesus Laiz <zheileman@users.noreply.github.com>
2017-11-21 16:20:25 +0000

    Make mutant happy

--- spec/forms/steps/safety_questions/substance_abuse_details_form_spec.rb
+++ spec/forms/steps/safety_questions/substance_abuse_details_form_spec.rb
@@ -6,18 +6,24 @@ RSpec.describe Steps::SafetyQuestions::SubstanceAbuseDetailsForm do
     substance_abuse_details: substance_abuse_details
   } }
   let(:c100_application) { instance_double(C100Application, substance_abuse_details: nil) }
-  let(:substance_abuse_details) { nil }
+  let(:substance_abuse_details) { 'details' }
 
   subject { described_class.new(arguments) }
 
   describe '#save' do
+    context 'when no c100_application is associated with the form' do
+      let(:c100_application) { nil }
+
+      it 'raises an error' do
+        expect { subject.save }.to raise_error(BaseForm::C100ApplicationNotFound)
+      end
+    end
+
     context 'validations' do
       it {should validate_presence_of(:substance_abuse_details)}
     end
 
     context 'when form is valid' do
-      let(:substance_abuse_details) { 'details' }
-
       it 'saves the record' do
         expect(c100_application).to receive(:update).with(
           substance_abuse_details: substance_abuse_details
