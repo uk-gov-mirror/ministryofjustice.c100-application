28acd945d767f123a4c44391f9bc4934e4a12a1c
Jesus Laiz <zheileman@users.noreply.github.com>
2017-11-21 11:49:02 +0000

    DRY HasOrders form using `SingleQuestionForm` (#45)
    Ok, it turned out this is the other form we can DRY making use of the
    concern, but eventually we will have MANY more forms with just one
    Yes/No question, and then this will be seen more useful.

--- app/forms/steps/court_orders/has_orders_form.rb
+++ app/forms/steps/court_orders/has_orders_form.rb
@@ -1,19 +1,9 @@
 module Steps
   module CourtOrders
     class HasOrdersForm < BaseForm
-      attribute :has_court_orders, YesNo
+      include SingleQuestionForm
 
-      validates_inclusion_of :has_court_orders, in: GenericYesNo.values
-
-      private
-
-      def persist!
-        raise C100ApplicationNotFound unless c100_application
-
-        c100_application.update(
-          has_court_orders: has_court_orders
-        )
-      end
+      yes_no_attribute :has_court_orders
     end
   end
 end
