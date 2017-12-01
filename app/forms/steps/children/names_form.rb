cb89989f86f5da43810ddbff3ce381fbcc65a0ed
Jesus Laiz <zheileman@users.noreply.github.com>
2017-12-01 10:20:58 +0000

    Add step `children names`
    This is a step using most of the code already created for the concern
    `CrudStep` but with some modifications in the form object to be able to
    create several children records at once, or update existing ones.
    
    Next children steps now need to be adapted to use the children records
    created by this step, to match the prototype, as these are now outdated.
    This will be done in a follow-up PR.

---
+++ app/forms/steps/children/names_form.rb
@@ -0,0 +1,40 @@
+module Steps
+  module Children
+    class NamesForm < BaseForm
+      attribute :names_attributes, Hash
+      attribute :new_name, StrippedString
+
+      validates_presence_of :new_name, if: :first_name?
+
+      private
+
+      def persist!
+        raise C100ApplicationNotFound unless c100_application
+
+        create_new_child_with_name
+        update_existing_children_names
+
+        true
+      end
+
+      def create_new_child_with_name
+        record_collection.create(name: new_name) unless new_name.blank?
+      end
+
+      def update_existing_children_names
+        names_attributes.each_value do |attrs|
+          record_collection.update(attrs.fetch('id'), attrs) unless attrs.fetch('name').blank?
+        end
+      end
+
+      def first_name?
+        return if c100_application.nil?
+        record_collection.empty?
+      end
+
+      def record_collection
+        @_record_collection ||= c100_application.children
+      end
+    end
+  end
+end
