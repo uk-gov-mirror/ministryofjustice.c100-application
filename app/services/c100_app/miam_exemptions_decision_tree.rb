47c16ecb43b80d42513a97a0baea74e0d179bfb5
Jesus Laiz <zheileman@users.noreply.github.com>
2018-01-19 11:27:06 +0000

    Add scaffolding for MIAM exemptions task

---
+++ app/services/c100_app/miam_exemptions_decision_tree.rb
@@ -0,0 +1,15 @@
+module C100App
+  class MiamExemptionsDecisionTree < BaseDecisionTree
+    def destination
+      return next_step if next_step
+
+      case step_name
+      # TODO: Put decision logic here
+      when :name
+        root_path
+      else
+        raise InvalidStep, "Invalid step '#{as || step_params}'"
+      end
+    end
+  end
+end
