ec104bc4b31adccb12ca9ed63b0959d4df19970b
Jesus Laiz <zheileman@users.noreply.github.com>
2018-01-23 16:48:17 +0000

    Ability to provide a custom name
    This will allow us to use a same Section presenter (same
    layout/partial) but render different bits of i18n copy.

--- app/presenters/summary/section_presenter.rb
+++ app/presenters/summary/section_presenter.rb
@@ -3,13 +3,16 @@ module Summary
     include Rails.application.routes.url_helpers
 
     attr_reader :c100_application
+    attr_reader :name
+
     alias_attribute :c100, :c100_application
 
-    def initialize(c100_application)
+    def initialize(c100_application, name: nil)
       @c100_application = c100_application
+      @name = name
     end
 
-    # Used by Rails to determine which partial to render
+    # Used by Rails to determine which partial to render. May be overridden in subclasses
     def to_partial_path
       'shared/section'
     end
