a1be58473d8ec35ee75a71af94ab35054cff338b
Jesus Laiz <zheileman@users.noreply.github.com>
2018-01-23 12:59:14 +0000

    Update Summary controller and views

--- app/controllers/steps/completion/summary_controller.rb
+++ app/controllers/steps/completion/summary_controller.rb
@@ -1,7 +1,18 @@
 module Steps
   module Completion
     class SummaryController < Steps::CompletionStepController
-      def show; end
+      def show
+        respond_to do |format|
+          format.html do
+            @presenter = Summary::HtmlPresenter.new(current_c100_application)
+          end
+
+          format.pdf do
+            @presenter = Summary::PdfPresenter.new(current_c100_application)
+            render @presenter.pdf_params
+          end
+        end
+      end
     end
   end
 end
--- spec/controllers/steps/completion/summary_controller_spec.rb
+++ spec/controllers/steps/completion/summary_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Steps::Completion::SummaryController, type: :controller do
-  it_behaves_like 'a show step controller'
+  it_behaves_like 'a summary step controller'
 end
