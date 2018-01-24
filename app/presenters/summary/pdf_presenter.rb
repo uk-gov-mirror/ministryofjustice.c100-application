a4b0fbee56823bda139c2b35adaff17bfa120858
Jesus Laiz <zheileman@users.noreply.github.com>
2018-01-24 11:53:32 +0000

    Add Help with fees section to the PDF
    This replaces the previous ExampleSection with a real section.

--- app/presenters/summary/pdf_presenter.rb
+++ app/presenters/summary/pdf_presenter.rb
@@ -9,7 +9,7 @@ module Summary
     def sections
       [
         Sections::HeaderSection.new(c100_application, name: :c100_form),
-        Sections::ExampleSection.new(c100_application),
+        Sections::HelpWithFees.new(c100_application),
       ].select(&:show?)
     end
 
