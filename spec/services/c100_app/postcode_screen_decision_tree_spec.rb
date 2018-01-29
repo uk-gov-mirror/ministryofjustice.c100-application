26419579fb099dc8b6656893b0a8f104d67a2487
Al Davidson <apdavidson@gmail.com>
2018-01-29 10:40:51 +0000

    fix mutant tests on postcode_screen_decision_tree

--- spec/services/c100_app/postcode_screen_decision_tree_spec.rb
+++ spec/services/c100_app/postcode_screen_decision_tree_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe C100App::PostcodeScreenDecisionTree do
-  let(:c100_application) { double('Object', children_postcodes: 'anything') }
+  let(:postcodes)        { 'anything' }
+  let(:c100_application) { double('Object', children_postcodes: postcodes) }
   let(:step_params)      { double('Step') }
   let(:next_step)        { nil }
   let(:as)               { nil }
@@ -16,19 +17,27 @@ RSpec.describe C100App::PostcodeScreenDecisionTree do
   
     context 'and no valid courts are found' do
       before do
-        allow_any_instance_of(C100App::CourtPostcodeChecker).to receive(:courts_for).and_return([])
+        allow_any_instance_of(C100App::CourtPostcodeChecker).to receive(:courts_for).with(postcodes).and_return([])
       end
       it { is_expected.to have_destination(:no_court_found, :show) }
     end
   
     context 'and at least one valid court is found' do
       before do
-        allow_any_instance_of(C100App::CourtPostcodeChecker).to receive(:courts_for).and_return(['i am a court'])
+        allow_any_instance_of(C100App::CourtPostcodeChecker).to receive(:courts_for).with(postcodes).and_return(['i am a court'])
       end
       it { is_expected.to have_destination('/steps/miam/consent_order', :edit) }
     end
 
     context 'when the postcode checker raises an error' do
+      before do
+        allow_any_instance_of(C100App::CourtPostcodeChecker).to receive(:courts_for).and_raise("expected exception for testing, please ignore")
+      end
+      it { is_expected.to have_destination(:error_but_continue, :show)}
+    end
+
+    context 'when the children_postcodes are nil' do
+      let(:postcodes){ nil }
       before do
         allow_any_instance_of(C100App::CourtPostcodeChecker).to receive(:courts_for).and_raise("expected exception for testing, please ignore")
       end
