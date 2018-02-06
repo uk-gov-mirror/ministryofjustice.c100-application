8ed7e6acf790d34eb7b9348123ac44fb1c0fd5bc
Al Davidson <apdavidson@gmail.com>
2018-02-06 11:16:20 +0000

    remove redundant commented-out specs, add uuid pk to screener answers, remove migration to port children_postcodes to screener_answers

--- db/migrate/20180205113216_add_screener_answers.rb
+++ db/migrate/20180205113216_add_screener_answers.rb
@@ -1,7 +1,8 @@
 class AddScreenerAnswers < ActiveRecord::Migration[5.1]
   def change
-    create_table :screener_answers do |t|
+    create_table :screener_answers, id: :uuid do |t|
       t.string :children_postcodes, null: true
+      t.timestamps null: false
     end
 
     add_reference :screener_answers, :c100_application, type: :uuid, foreign_key: true
