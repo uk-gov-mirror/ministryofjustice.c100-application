49969bdec205000c9b9e04ee611da78c556372ff
Jesus Laiz <zheileman@users.noreply.github.com>
2018-02-08 16:54:38 +0000

    Expose a method to check for C8
    This is going to be used in several places and it makes sense to expose
    a method in the model itself to query for the information, and also in
    case the conditions that triggers the C8 change in the future.

--- spec/models/c100_application_spec.rb
+++ spec/models/c100_application_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe C100Application, type: :model do
   subject { described_class.new(attributes) }
   let(:attributes) { {} }
+
+  describe '#confidentiality_enabled?' do
+    context 'for `yes` values' do
+      let(:attributes) { {address_confidentiality: 'yes'} }
+      it { expect(subject.confidentiality_enabled?).to eq(true) }
+    end
+
+    context 'for `no` values' do
+      let(:attributes) { {address_confidentiality: 'no'} }
+      it { expect(subject.confidentiality_enabled?).to eq(false) }
+    end
+  end
 end
