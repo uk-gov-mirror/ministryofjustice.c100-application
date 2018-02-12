a2bff36dddda2c4cdcbe0ce27b13abdadab1a533
Jesus Laiz <zheileman@users.noreply.github.com>
2018-02-12 11:40:00 +0000

    Add explanation about fragile tests
    I’ve added the explanation only in those specs with a high number of
    rows as these are more prone to be affected with code changes.

--- spec/presenters/summary/sections/other_children_details_spec.rb
+++ spec/presenters/summary/sections/other_children_details_spec.rb
@@ -34,6 +34,11 @@ module Summary
       end
     end
 
+    # The following tests can be fragile, but on purpose. During the development phase
+    # we have to update the tests each time we introduce a new row or remove another.
+    # But once it is finished and stable, it will raise a red flag if it ever gets out
+    # of sync, which means a quite solid safety net for any maintainers in the future.
+    #
     describe '#answers' do
       before do
         allow(relationships).to receive_message_chain(:where, :pluck).and_return(['mother'])
