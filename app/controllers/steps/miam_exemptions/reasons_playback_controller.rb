36d137680914fd6eb146abe935115fa98a486239
Jesus Laiz <zheileman@users.noreply.github.com>
2018-02-19 08:53:47 +0000

    Implement the playback of the exemptions

--- app/controllers/steps/miam_exemptions/reasons_playback_controller.rb
+++ app/controllers/steps/miam_exemptions/reasons_playback_controller.rb
@@ -1,7 +1,11 @@
 module Steps
   module MiamExemptions
     class ReasonsPlaybackController < Steps::MiamExemptionsStepController
-      def show; end
+      def show
+        @presenter = MiamExemptionsPresenter.new(
+          current_c100_application.miam_exemption
+        )
+      end
     end
   end
 end
---
+++ spec/presenters/miam_exemptions_presenter_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+RSpec.describe MiamExemptionsPresenter do
+  subject { described_class.new(miam_exemption) }
+
+  let(:miam_exemption) { MiamExemption.new(domestic: domestic_exemptions) }
+  let(:domestic_exemptions) { %w(police_conviction specialist_examination) }
+
+  describe '#exemption_groups' do
+    it { expect(subject.exemption_groups).to eq([:domestic, :protection, :urgency, :adr, :misc]) }
+  end
+
+  describe '#exemptions' do
+    let(:exemptions) { subject.exemptions }
+
+    before do
+      # We mock just one group to simplify the tests, as the behaviour is identical
+      # for one or for five groups.
+      allow(subject).to receive(:exemption_groups).and_return([:domestic])
+    end
+
+    it 'retrieves the exemptions from the groups' do
+      expect(exemptions.size).to eq(1)
+
+      expect(exemptions[0].group_name).to eq(:domestic)
+      expect(exemptions[0].collection).to eq(domestic_exemptions)
+      expect(exemptions[0].to_partial_path).to eq('steps/miam_exemptions/shared/exemption')
+    end
+
+    context 'filter out `group_xxx` items' do
+      let(:domestic_exemptions) { %w(group_police police_conviction) }
+
+      it 'retrieves the exemptions from the groups' do
+        expect(exemptions[0].collection).to eq(['police_conviction'])
+      end
+    end
+
+    context 'filter out `xxx_none` items' do
+      let(:domestic_exemptions) { %w(domestic_none) }
+
+      it 'retrieves the exemptions from the groups' do
+        expect(exemptions.size).to eq(0)
+      end
+    end
+  end
+end
