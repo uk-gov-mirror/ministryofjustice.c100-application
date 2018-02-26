0d7b32a9b371a1e195765ad2030d596ecd609d91
Jesus Laiz <zheileman@users.noreply.github.com>
2018-02-26 10:49:11 +0000

    Add `substance abuse` section

---
+++ app/presenters/summary/sections/c1a_concerns_substance.rb
@@ -0,0 +1,18 @@
+module Summary
+  module Sections
+    class C1aConcernsSubstance < BaseSectionPresenter
+      def name
+        :c1a_concerns_details
+      end
+
+      def answers
+        return [] unless c100.substance_abuse.eql?(GenericYesNo::YES.to_s)
+
+        [
+          Answer.new(:c1a_abuse_type, :substance_abuse),
+          FreeTextAnswer.new(:c1a_abuse_details, c100.substance_abuse_details),
+        ]
+      end
+    end
+  end
+end
