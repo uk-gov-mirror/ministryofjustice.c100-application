50eec3ebc2d52edbbe22b9f942a2ad6bc2475cfc
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-01 17:05:46 +0000

    Preparing warden helper
    This is used to mock a logged-in user in tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require_relative '../spec/support/view_spec_helpers'
+require_relative '../spec/helpers/authentication_helpers_spec'
 require 'database_cleaner'
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -18,8 +19,10 @@ RSpec.configure do |config|
   config.include(ActiveSupport::Testing::TimeHelpers)
 
   config.include(ViewSpecHelpers, type: :helper)
+
   config.include(Devise::Test::ControllerHelpers, type: :view)
   config.include(Devise::Test::ControllerHelpers, type: :controller)
+  config.include(AuthenticationHelpers, type: :controller)
 
   config.before(:each, type: :helper) { initialize_view_helpers(helper) }
 
