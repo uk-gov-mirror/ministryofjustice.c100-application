ff5751276c27eab7c4351a9860542cbfb1693099
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-02 13:27:53 +0000

    Create basic emails, and templates config
    We had already a NotifyMailer from TaxTribs, but I’ve removed unneeded
    code (at least for now) and improved the way we handle common
    personalisation like the service name.
    
    Also, get rid of ENV variables for template IDs, as they are not
    secrets, we can declare them in a config file for more easy maintenance.

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,37 +1,63 @@
 class NotifyMailer < GovukNotifyRails::Mailer
   rescue_from Exception, with: :log_errors
 
+  before_action do
+    @service_name = I18n.translate!(:service_name, scope: 'dictionary')
+    @template_ids = Rails.configuration.govuk_notify_templates
+  end
+
   # When logging exceptions, filter the following keys from the personalisation hash
   FILTERED = '[FILTERED]'.freeze
   PERSONALISATION_ERROR_FILTER = [
-    :recipient_name,
-    :reset_url
+    :reset_password_url
   ].freeze
 
-  # Define methods as usual, and set the template and personalisation, if needed,
-  # then just use mail() as with any other ActionMailer, with the recipient email
-
-  # Triggered automatically by Devise when the user resets its password
+  # Triggered automatically by Devise when the user resets its password.
+  # Do not change the method name unless you configure Devise with the new name.
   def reset_password_instructions(user, token, _opts = {})
-    set_template(ENV.fetch('NOTIFY_RESET_PASSWORD_TEMPLATE_ID'))
+    set_template(:reset_password)
 
     set_personalisation(
-      reset_url: edit_user_password_url(reset_password_token: token)
+      reset_password_url: edit_user_password_url(reset_password_token: token),
     )
 
     mail(to: user.email)
   end
 
-  # Triggered automatically by Devise when the user changes its password
+  # Triggered automatically by Devise when the user changes its password.
+  # Do not change the method name unless you configure Devise with the new name.
   def password_change(user, _opts = {})
-    set_template(ENV.fetch('NOTIFY_CHANGE_PASSWORD_TEMPLATE_ID'))
+    set_template(:change_password)
 
-    # set_personalisation(
-    #   portfolio_url: users_cases_url
-    # )
+    set_personalisation(
+      drafts_url: users_drafts_url,
+    )
 
     mail(to: user.email)
   end
+
+  def application_saved_confirmation(c100_application)
+    set_template(:application_saved)
+
+    set_personalisation(
+      resume_draft_link: resume_users_draft_url(c100_application),
+      draft_expire_in_days: Rails.configuration.x.drafts.expire_in_days,
+    )
+
+    mail(to: c100_application.user.email)
+  end
+
+  protected
+
+  # rubocop:disable Naming/AccessorMethodName
+  def set_template(name)
+    super(@template_ids.fetch(name))
+  end
+
+  def set_personalisation(personalisation)
+    super({ service_name: @service_name }.merge(personalisation))
+  end
+  # rubocop:enable Naming/AccessorMethodName
 
   private
 
---
+++ spec/mailers/notify_mailer_spec.rb
@@ -0,0 +1,97 @@
+require 'spec_helper'
+
+RSpec.describe NotifyMailer, type: :mailer do
+  let(:c100_application) {
+    C100Application.new(
+      id: '4a362e1c-48eb-40e3-9458-a31ead3f30a4',
+    )
+  }
+  let(:user) { instance_double(User, email: 'test@example.com') }
+
+  before do
+    allow(c100_application).to receive(:user).and_return(user)
+
+    allow(
+      Rails.configuration
+    ).to receive(:govuk_notify_templates).and_return(
+      reset_password: 'reset_password_template_id',
+      change_password: 'change_password_template_id',
+      application_saved: 'application_saved_template_id',
+    )
+  end
+
+  describe '#application_saved_confirmation' do
+    let(:mail) { described_class.application_saved_confirmation(c100_application) }
+
+    it_behaves_like 'a Notify mail', template_id: 'application_saved_template_id'
+
+    it { expect(mail.to).to eq(['test@example.com']) }
+
+    it 'has the right personalisation' do
+      expect(mail.govuk_notify_personalisation).to eq({
+        service_name: 'Apply to court about child arrangements',
+        resume_draft_link: 'https://c100.justice.uk/users/drafts/4a362e1c-48eb-40e3-9458-a31ead3f30a4/resume',
+        draft_expire_in_days: 14,
+      })
+    end
+  end
+
+  describe '#reset_password_instructions' do
+    let(:mail)  { described_class.reset_password_instructions(user, token) }
+    let(:token) { '0xDEADBEEF' }
+
+    it_behaves_like 'a Notify mail', template_id: 'reset_password_template_id'
+
+    it { expect(mail.to).to eq(['test@example.com']) }
+
+    it 'has the right keys' do
+      expect(mail.govuk_notify_personalisation).to eq({
+        service_name: 'Apply to court about child arrangements',
+        reset_password_url: 'https://c100.justice.uk/users/password/edit?reset_password_token=0xDEADBEEF',
+      })
+    end
+  end
+
+  describe '#password_change' do
+    let(:mail) { described_class.password_change(user) }
+
+    it_behaves_like 'a Notify mail', template_id: 'change_password_template_id'
+
+    it { expect(mail.to).to eq(['test@example.com']) }
+
+    it 'has the right keys' do
+      expect(mail.govuk_notify_personalisation).to eq({
+        service_name: 'Apply to court about child arrangements',
+        drafts_url: 'https://c100.justice.uk/users/drafts',
+      })
+    end
+  end
+
+  context 'capturing unexpected errors' do
+    let(:mail) { described_class.reset_password_instructions(nil, 'token') }
+
+    it 'should report the exception' do
+      expect(Rails.logger).to receive(:info)
+      expect(Raven).to receive(:capture_exception)
+      expect(Raven).to receive(:extra_context).with(
+        {
+          template_id: 'reset_password_template_id',
+          personalisation: {
+            service_name: 'Apply to court about child arrangements',
+            reset_password_url: '[FILTERED]',
+          }
+        }
+      )
+
+      mail.deliver_now
+    end
+  end
+
+  describe 'personalisation logging filter' do
+    it {
+      expect(
+        described_class::PERSONALISATION_ERROR_FILTER
+      ).to match_array([:reset_password_url])
+    }
+  end
+end
