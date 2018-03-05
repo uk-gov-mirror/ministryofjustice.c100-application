62d653e9be18553079586ce234eb3fe883a47779
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-05 13:35:16 +0000

    Add `SavepointStep` concern
    This service, as opposite to Tax Tribs, has a peculiarity that requires
    a bit of extra code around the save&return, which is a “Screener”,
    composed of several steps.
    
    This screener goes before the actual C100 application steps start, and
    behave a bit different in the sense the screener will not have
    Save&Return, and also once completed, we shouldn’t take the user back
    to the screener again.
    
    This concern will mark the exact step where the Save&Return triggers
    and the application is marked as `in_progress` meaning it passed the
    screening.

--- spec/controllers/steps/miam/consent_order_controller_spec.rb
+++ spec/controllers/steps/miam/consent_order_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Steps::Miam::ConsentOrderController, type: :controller do
+  it_behaves_like 'a savepoint step controller'
   it_behaves_like 'an intermediate step controller', Steps::Miam::ConsentOrderForm, C100App::MiamDecisionTree
 end
