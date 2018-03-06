a2cb8d4b00d56055b58c3c677e32ac0ed0bae7c0
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-06 12:57:14 +0000

    Copy changes to Litigation Capacity step
    A few copy changes and reorder the text areas.

--- app/presenters/summary/sections/litigation_capacity.rb
+++ app/presenters/summary/sections/litigation_capacity.rb
@@ -13,8 +13,8 @@ module Summary
         [
           Answer.new(:reduced_litigation_capacity, c100.reduced_litigation_capacity),
           FreeTextAnswer.new(:participation_capacity_details, c100.participation_capacity_details),
-          FreeTextAnswer.new(:participation_referral_or_assessment_details, c100.participation_referral_or_assessment_details),
           FreeTextAnswer.new(:participation_other_factors_details, c100.participation_other_factors_details),
+          FreeTextAnswer.new(:participation_referral_or_assessment_details, c100.participation_referral_or_assessment_details),
         ].select(&:show?)
       end
     end
