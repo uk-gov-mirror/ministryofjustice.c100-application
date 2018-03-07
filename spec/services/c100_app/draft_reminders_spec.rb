b7bf7a6d212cc96082320b359359ec0c897a6dee
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-07 10:58:55 +0000

    Implement the reminder mechanism
    This is mostly the same solution we had in Tax Tribs, with minor changes.
    
    We setup 2 draft expire reminders, one to be sent when 5 days left and another one to be sent when 1 day left.
    The mechanism is flexible enough to setup as many reminders as we want to trigger at specific moments in time.

---
+++ spec/services/c100_app/draft_reminders_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe C100App::DraftReminders do
+  subject { described_class.new(rule_set: rule_set) }
+
+  let(:rule_set) {
+    instance_double(
+      C100App::ReminderRuleSet,
+      email_template_name: :template_name,
+      status_transition_to: :another_status
+    )
+  }
+  let(:c100_application) { instance_double(C100Application) }
+
+  describe '#run' do
+    let(:mailer_double) { double.as_null_object }
+
+    before do
+      allow(rule_set).to receive(:find_each).and_yield(c100_application)
+      allow(NotifyMailer).to receive(:draft_expire_reminder).with(c100_application, :template_name).and_return(mailer_double)
+    end
+
+    it 'should send the email and update the C100 application status' do
+      expect(mailer_double).to receive(:deliver_now)
+      expect(c100_application).to receive(:update).with(status: :another_status)
+      subject.run
+    end
+  end
+end
---
+++ spec/services/c100_app/reminder_rule_set_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe C100App::ReminderRuleSet do
+  describe '.first_reminder' do
+    subject { described_class.first_reminder }
+
+    it { expect(subject.created_days_ago).to eq(9) }
+    it { expect(subject.status).to eq(:in_progress) }
+    it { expect(subject.status_transition_to).to eq(:first_reminder_sent) }
+    it { expect(subject.email_template_name).to eq(:draft_first_reminder) }
+  end
+
+  describe '.last_reminder' do
+    subject { described_class.last_reminder }
+
+    it { expect(subject.created_days_ago).to eq(13) }
+    it { expect(subject.status).to eq(:first_reminder_sent) }
+    it { expect(subject.status_transition_to).to eq(:last_reminder_sent) }
+    it { expect(subject.email_template_name).to eq(:draft_last_reminder) }
+  end
+
+  describe '#find_each' do
+    let(:dummy_config) do
+      {
+        created_days_ago: 3,
+        status: :test_status,
+        status_transition_to: :another_status,
+        email_template_name: :template_name
+      }
+    end
+    let(:finder_double) { double.as_null_object }
+
+    subject { described_class.new(dummy_config) }
+
+    before do
+      travel_to Time.now
+    end
+
+    it 'filters the c100 applications' do
+      expect(C100Application).to receive(:with_owner).and_return(finder_double)
+      expect(finder_double).to receive(:where).with(status: :test_status).and_return(finder_double)
+      expect(finder_double).to receive(:where).with('created_at <= ?', 3.days.ago).and_return(finder_double)
+      subject.find_each
+    end
+
+    it 'it returns an enumerator' do
+      expect(subject.find_each).to be_an(Enumerator)
+    end
+  end
+end
