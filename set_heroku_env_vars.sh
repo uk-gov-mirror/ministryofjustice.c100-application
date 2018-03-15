85ff38e611b84d2c201f248ccc98d6eb67d36a9d
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-15 13:36:20 +0000

    Do not rely on `HEROKU_APP_NAME` env var
    This env var is only set if a specific plugin is enabled on Heroku. If we ever move out of Heroku this will not make sense. Better to have a specific.

--- set_heroku_env_vars.sh
+++ set_heroku_env_vars.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 heroku config:set \
-  EXTERNAL_URL=https://c100-demo.herokuapp.com \
+  EXTERNAL_URL=https://c100-staging.herokuapp.com \
   RACK_ENV=production \
   RAILS_ENV=production \
   RAILS_LOG_TO_STDOUT=enabled \
@@ -11,6 +11,7 @@ heroku config:set \
   RAILS_SERVE_STATIC_FILES=enabled \
   SECRET_KEY_BASE=$(bundle exec rails secret) \
   GOVUK_NOTIFY_API_KEY=DUMMYVALUE \
+  HTTP_AUTH_ENABLED=1 \
   HTTP_AUTH_USER=${HTTP_AUTH_USER} \
   HTTP_AUTH_PASSWORD=${HTTP_AUTH_PASSWORD} \
   LANG=en_US.UTF-8
