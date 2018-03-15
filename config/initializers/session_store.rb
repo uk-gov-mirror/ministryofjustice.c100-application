6cc5b371df6f779c9692ecceffc127445d5749c9
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-15 11:44:49 +0000

    Make session expire when browser closes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-Rails.application.config.session_store :cookie_store,
-  key:          '_c100_application_session',
-  expire_after: Rails.configuration.x.session.expires_in_minutes.minutes
+Rails.application.config.session_store :cookie_store, key: '_c100_application_session'
