cbdea14d300e509496d90372f4f7c4d6a1174d21
Jesus Laiz <zheileman@users.noreply.github.com>
2018-03-21 17:31:24 +0100

    Copy updates in MIAM exemptions
    And re-organise the Urgency exemptions.

--- app/value_objects/urgency_exemptions.rb
+++ app/value_objects/urgency_exemptions.rb
@@ -2,13 +2,8 @@ class UrgencyExemptions < ValueObject
   VALUES = [
     RISK_APPLICANT = new(:risk_applicant),
     UNREASONABLE_HARDSHIP = new(:unreasonable_hardship),
-
-    GROUP_RISK = new(:group_risk),
-    RISK = [
-      new(:risk_children),
-      new(:risk_unlawful_removal_retention)
-    ].freeze,
-
+    RISK_CHILDREN = new(:risk_children),
+    RISK_UNLAWFUL_REMOVAL_RETENTION = new(:risk_unlawful_removal_retention),
     MISCARRIAGE_JUSTICE = new(:miscarriage_justice),
     IRRETRIEVABLE_PROBLEMS = new(:irretrievable_problems),
     INTERNATIONAL_PROCEEDINGS = new(:international_proceedings),
