8ff83a13ef703c44ed51bf48228f9ec21fb26918
Jesus Laiz <zheileman@users.noreply.github.com>
2018-04-10 16:54:00 +0100

    Reset fields on `miam_certification` change

--- app/forms/steps/miam/certification_form.rb
+++ app/forms/steps/miam/certification_form.rb
@@ -3,7 +3,11 @@ module Steps
     class CertificationForm < BaseForm
       include SingleQuestionForm
 
-      yes_no_attribute :miam_certification
+      yes_no_attribute :miam_certification,
+                       reset_when_no: [
+                         Steps::Miam::CertificationDateForm,
+                         Steps::Miam::CertificationDetailsForm,
+                       ]
     end
   end
 end
--- spec/forms/steps/miam/certification_form_spec.rb
+++ spec/forms/steps/miam/certification_form_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Steps::Miam::CertificationForm do
-  it_behaves_like 'a yes-no question form', attribute_name: :miam_certification
+  it_behaves_like 'a yes-no question form',
+                  attribute_name: :miam_certification,
+                  reset_when_no: [
+                    :miam_certification_date,
+                    :miam_certification_number,
+                    :miam_certification_service_name,
+                    :miam_certification_sole_trader_name,
+                  ]
 end
