4657efb3f4ebfc0d7baa7fbbaaacb71c5c4541f2
Jesus Laiz <zheileman@users.noreply.github.com>
2018-04-10 16:53:38 +0100

    Reset fields on `child_protection_cases` change

--- app/forms/steps/miam/child_protection_cases_form.rb
+++ app/forms/steps/miam/child_protection_cases_form.rb
@@ -3,7 +3,15 @@ module Steps
     class ChildProtectionCasesForm < BaseForm
       include SingleQuestionForm
 
-      yes_no_attribute :child_protection_cases
+      yes_no_attribute :child_protection_cases,
+                       reset_when_yes: [
+                         :miam_acknowledgement,
+                         :miam_attended,
+                         :miam_exemption_claim,
+                         :miam_certification,
+                         Steps::Miam::CertificationDateForm,
+                         Steps::Miam::CertificationDetailsForm,
+                       ]
     end
   end
 end
