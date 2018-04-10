c6e3dc5f86ad04847372528a5a8a63c4548ce58c
Jesus Laiz <zheileman@users.noreply.github.com>
2018-04-10 17:01:11 +0100

    Reset fields on some abduction questions change

--- spec/forms/steps/abduction/children_have_passport_form_spec.rb
+++ spec/forms/steps/abduction/children_have_passport_form_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 RSpec.describe Steps::Abduction::ChildrenHavePassportForm do
   let(:arguments) { {
     c100_application: c100_application,
-    children_have_passport: 'yes'
+    children_have_passport: children_have_passport,
   } }
 
   let(:c100_application) { instance_double(C100Application) }
+  let(:children_have_passport) { 'yes' }
 
   subject { described_class.new(arguments) }
 
@@ -20,5 +21,20 @@ RSpec.describe Steps::Abduction::ChildrenHavePassportForm do
                     expected_attributes: {
                       children_have_passport: GenericYesNo::YES,
                     }
+
+    context 'reset of other attributes' do
+      let(:children_have_passport) { 'no' }
+
+      it_behaves_like 'a has-one-association form',
+                      association_name: :abduction_detail,
+                      expected_attributes: {
+                        children_have_passport: GenericYesNo::NO,
+                        children_multiple_passports: nil,
+                        passport_possession_mother: nil,
+                        passport_possession_father: nil,
+                        passport_possession_other: nil,
+                        passport_possession_other_details: nil,
+                      }
+    end
   end
 end
