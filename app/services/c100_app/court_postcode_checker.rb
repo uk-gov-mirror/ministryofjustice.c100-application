f2c466955016ff8d5f8f7dd2a79d2597b4fbbc00
Jesus Laiz <zheileman@users.noreply.github.com>
2018-04-20 14:03:26 +0100

    Revert "make eligibility check & destination address only consider closest court (#365)"
    This reverts commit 6400df85532101564ce5c66ce26a9f311de8c40a.

--- app/services/c100_app/court_postcode_checker.rb
+++ app/services/c100_app/court_postcode_checker.rb
@@ -7,8 +7,7 @@ module C100App
       "reading-county-court-and-family-court",
       "guildford-county-court-and-family-court",
       "milton-keynes-county-court-and-family-court",
-      "watford-county-court-and-family-court",
-      "slough-county-court-and-family-court",
+      "watford-county-court-and-family-court"
     ].freeze
 
     # Separate multiple postcodes/postcode areas by "\n"
@@ -30,11 +29,9 @@ module C100App
     private
 
     def choose_from(possible_courts)
-      slug = nil
-      first_with_slug = possible_courts.find do |court|
-        slug = court[:slug] || court['slug']
+      possible_courts.find do |court|
+        COURT_SLUGS_USING_THIS_APP.include?(court['slug'] || court[:slug])
       end
-      first_with_slug if COURT_SLUGS_USING_THIS_APP.include?(slug)
     end
   end
 end
