be262fb05f3c0db28fede7a7816a56909d1c91a6
Jesus Laiz <zheileman@users.noreply.github.com>
2018-05-01 12:41:30 +0100

    Reset abuse concerns when no longer apply
    This is a bit of an edge case, but once we have the `check your answers`, it could happen.

--- spec/forms/steps/safety_questions/other_abuse_form_spec.rb
+++ spec/forms/steps/safety_questions/other_abuse_form_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Steps::SafetyQuestions::OtherAbuseForm do
-  it_behaves_like 'a yes-no question form', attribute_name: :other_abuse
+  it_behaves_like 'a yes-no question form',
+                  attribute_name: :other_abuse,
+                  reset_when_no: [:abuse_concern_ids]
 end
