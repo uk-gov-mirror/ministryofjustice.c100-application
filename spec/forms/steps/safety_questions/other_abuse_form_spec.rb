1b24f60c71faf2b9c296cd375c0fb4981b73d7e2
Jesus Laiz <zheileman@users.noreply.github.com>
2018-05-01 15:14:13 +0100

    Reset more C1A fields
    There are a few more fields that we need to reset.

--- spec/forms/steps/safety_questions/other_abuse_form_spec.rb
+++ spec/forms/steps/safety_questions/other_abuse_form_spec.rb
@@ -3,5 +3,13 @@ require 'spec_helper'
 RSpec.describe Steps::SafetyQuestions::OtherAbuseForm do
   it_behaves_like 'a yes-no question form',
                   attribute_name: :other_abuse,
-                  reset_when_no: [:abuse_concern_ids]
+                  reset_when_no: [
+                    :abuse_concern_ids,
+                    :has_court_orders,
+                    :court_order,
+                    :concerns_contact_type,
+                    :concerns_contact_other,
+                    :protection_orders,
+                    :protection_orders_details,
+                  ]
 end
