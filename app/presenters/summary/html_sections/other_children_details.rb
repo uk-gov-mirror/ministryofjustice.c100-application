5f9fb7d2a65501f5076f33afbc4d75408cd5eb92
Jesus Laiz <zheileman@users.noreply.github.com>
2018-05-08 12:40:32 +0100

    Reorganise children sections in CYA
    This follows more closely the pattern we used for the rest of people (applicants, respondents...) and also simplify the code.
    
    The relationships has been removed from the children sections, and will be reintroduced in each of the applicant/respondent sections, as structurally, it is were they belong when the user is completing their application online.

--- app/presenters/summary/html_sections/other_children_details.rb
+++ app/presenters/summary/html_sections/other_children_details.rb
@@ -5,46 +5,29 @@ module Summary
         :other_children_details
       end
 
-      def answers
-        return [Separator.not_applicable] if other_children.empty?
+      def record_collection
+        c100.other_children
+      end
 
+      def answers
         [
-          other_children_details
+          Answer.new(
+            :has_other_children,
+            c100.has_other_children,
+            change_path: edit_steps_children_has_other_children_path
+          ),
+          super,
         ].flatten.select(&:show?)
       end
 
-      private
+      protected
 
-      def other_children_details
-        other_children.map.with_index(1) do |child, index|
-          [
-            Separator.new(:other_child_index_title, index: index),
-            personal_details(child),
-            relationships(child),
-          ]
-        end
+      def names_path
+        edit_steps_other_children_names_path(id: '')
       end
 
-      def edit_children_names_path
-        '/steps/other_children/names'
-      end
-
-      def edit_child_details_path(child, field_stub)
-        edit_steps_other_children_personal_details_path(
-          child.id,
-          anchor: anchor(field_stub)
-        )
-      end
-
-      def anchor(field_stub)
-        format(
-          "steps_children_personal_details_form_%<field_stub>s",
-          field_stub: field_stub
-        )
-      end
-
-      def other_children
-        @_other_children ||= c100.other_children
+      def personal_details_path(child)
+        edit_steps_other_children_personal_details_path(child)
       end
     end
   end
--- spec/presenters/summary/html_sections/children_details_spec.rb
+++ spec/presenters/summary/html_sections/children_details_spec.rb
@@ -5,32 +5,23 @@ module Summary
     let(:c100_application) {
       instance_double(C100Application,
         children: [child],
-        other_children: [],
-        applicants: [],
-        respondents: [],
-        has_other_children: 'yes',
       )
     }
 
     let(:child) {
       instance_double(Child,
-        id: '1234',
+        to_param: 'uuid-123',
         full_name: 'name',
         dob: dob,
         age_estimate: age_estimate,
         gender: 'female',
         child_order: child_order,
-        relationships: relationships,
       )
     }
 
     let(:dob) { Date.new(2018, 1, 20) }
     let(:age_estimate) { nil }
     let(:child_order) { instance_double(ChildOrder, orders: ['an_order']) }
-    let(:relationship){ instance_double(Relationship, relation: 'mother') }
-    let(:relationships) do
-      double('relationships', pluck: relationship, first: relationship)
-    end
 
     subject { described_class.new(c100_application) }
 
@@ -48,58 +39,40 @@ module Summary
     # of sync, which means a quite solid safety net for any maintainers in the future.
     #
     describe '#answers' do
-      before do
-        allow(subject).to receive(:first_related_person_id).and_return('5678')
-        allow(subject).to receive(:relation_to_child).and_return('mother')
-        allow(subject).to receive(:relationship).and_return(relationships)
-      end
-
       it 'has the correct number of rows' do
-        expect(answers.count).to eq(8)
+        expect(answers.count).to eq(4)
       end
 
       it 'has the correct rows in the right order' do
         expect(answers[0]).to be_an_instance_of(Separator)
-        expect(answers[0].title).to eq(:child_index_title)
+        expect(answers[0].title).to eq('children_details_index_title')
         expect(answers[0].i18n_opts).to eq({index: 1})
 
         expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[1].question).to eq(:child_full_name)
+        expect(answers[1].question).to eq(:person_full_name)
         expect(answers[1].value).to eq('name')
-        expect(answers[1].change_path).to eq('/steps/children/names')
+        expect(answers[1].change_path).to eq('/steps/children/names/')
 
-        expect(answers[2]).to be_an_instance_of(DateAnswer)
-        expect(answers[2].question).to eq(:child_dob)
-        expect(answers[2].value).to eq(Date.new(2018, 1, 20))
-        expect(answers[2].change_path).to eq('/steps/children/personal_details/1234#steps_children_personal_details_form_dob_dd')
+        expect(answers[2]).to be_an_instance_of(AnswersGroup)
+        expect(answers[2].name).to eq(:person_personal_details)
+        expect(answers[2].change_path).to eq('/steps/children/personal_details/uuid-123')
+        expect(answers[2].answers.count).to eq(2)
 
-        expect(answers[3]).to be_an_instance_of(Answer)
-        expect(answers[3].question).to eq(:child_sex)
-        expect(answers[3].value).to eq('female')
-        expect(answers[3].change_path).to eq('/steps/children/personal_details/1234#steps_children_personal_details_form_gender_female')
+          # personal_details group answers ###
+          details = answers[2].answers
 
-        expect(answers[4]).to be_an_instance_of(MultiAnswer)
-        expect(answers[4].question).to eq(:child_applicants_relationship)
-        expect(answers[4].value).to eq('mother')
-        expect(c100_application).to have_received(:applicants).at_least(:once)
-        expect(answers[4].change_path).to eq("/steps/applicant/relationship/5678/child/1234")
+          expect(details[0]).to be_an_instance_of(DateAnswer)
+          expect(details[0].question).to eq(:person_dob)
+          expect(details[0].value).to eq(Date.new(2018, 1, 20))
 
-        expect(answers[5]).to be_an_instance_of(MultiAnswer)
-        expect(answers[5].question).to eq(:child_respondents_relationship)
-        expect(answers[5].value).to eq('mother')
-        expect(c100_application).to have_received(:respondents).at_least(:once)
-        expect(answers[5].change_path).to eq("/steps/respondent/relationship/5678/child/1234")
+          expect(details[1]).to be_an_instance_of(Answer)
+          expect(details[1].question).to eq(:person_sex)
+          expect(details[1].value).to eq('female')
 
-        expect(answers[6]).to be_an_instance_of(MultiAnswer)
-        expect(answers[6].question).to eq(:child_orders)
-        expect(answers[6].value).to eq(['other_issue'])
-        expect(answers[6].change_path).to eq('/steps/children/orders/1234')
-
-        expect(answers[7]).to be_an_instance_of(Answer)
-        expect(answers[7].question).to eq(:has_other_children)
-        expect(c100_application).to have_received(:has_other_children)
-        expect(answers[7].change_path).to eq('/steps/children/has_other_children')
-
+        expect(answers[3]).to be_an_instance_of(MultiAnswer)
+        expect(answers[3].question).to eq(:child_orders)
+        expect(answers[3].value).to eq(['other_issue'])
+        expect(answers[3].change_path).to eq('/steps/children/orders/uuid-123')
       end
 
       context 'when `dob` is nil' do
@@ -107,9 +80,13 @@ module Summary
         let(:age_estimate) { 18 }
 
         it 'uses the age estimate' do
-          expect(answers[2]).to be_an_instance_of(FreeTextAnswer)
-          expect(answers[2].question).to eq(:child_age_estimate)
-          expect(answers[2].value).to eq(18)
+          expect(answers[2]).to be_an_instance_of(AnswersGroup)
+
+          details = answers[2].answers
+
+          expect(details[0]).to be_an_instance_of(FreeTextAnswer)
+          expect(details[0].question).to eq(:person_age_estimate)
+          expect(details[0].value).to eq(18)
         end
       end
     end
