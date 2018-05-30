c65c843fa7c0659de5a984c140f2f0a625c36bd6
Al Davidson <apdavidson@gmail.com>
2018-05-30 11:55:01 +0100

    submitting in dev now opens sent emails in a browser

--- app/models/email_submission.rb
+++ app/models/email_submission.rb
@@ -2,10 +2,10 @@ class EmailSubmission < ApplicationRecord
   belongs_to :c100_application
 
   def initialize(params = {})
-    @email_copy_to = params[:email_copy_to]
     @c100_application = params[:c100_application]
+    @email_copy_to = @c100_application.try(:receipt_email)
     @to_address = @c100_application.try(:court_from_screener_answers).try(:email)
-    @from = ENV['SUBMISSION_EMAIL_FROM']
+    @from = ENV['SUBMISSION_EMAIL_FROM'] || 'from@example.com'
     super
   end
 
@@ -23,9 +23,10 @@ class EmailSubmission < ApplicationRecord
       attachment: pdf_file_path
     ).deliver_now!
     self.sent_at = Time.now.utc
+    self.to_address = @to_address
     self.message_id = response.message_id
 
-    if self.email_me_a_copy.to_s.eql? GenericYesNo::YES
+    if @email_copy_to.present?
       # if the user hits reply, it should go to the court
       response = EmailSubmissionMailer.copy_to_user(
         c100_application: c100_application,
