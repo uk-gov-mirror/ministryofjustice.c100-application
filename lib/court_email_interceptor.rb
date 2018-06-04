ed0c0616afadd962357a6f7d50a6ae0195079e0e
Jesus Laiz <zheileman@users.noreply.github.com>
2018-06-04 14:25:55 +0100

    Fix interceptor when there is no reply-to address
    Cancelling the delivery with `message.perform_deliveries = false` was not having an effect, so to not spam courts, we will send instead this emails to our google group.
    
    If the reply-to is present, we continue using this.

--- lib/court_email_interceptor.rb
+++ lib/court_email_interceptor.rb
@@ -23,14 +23,10 @@ class CourtEmailInterceptor
     def intercept_email!(message)
       # If the user chose to receive a confirmation receipt, we will use this
       # `reply-to` address as the new recipient of the intercepted email.
-      # Otherwise we will just stop the delivery completely.
+      # Otherwise we will default to the `from` address.
       #
-      if message.reply_to.present?
-        message.subject = "#{message.subject} | (Original recipient: #{message.to.join(',')})"
-        message.to = message.reply_to
-      else
-        message.perform_deliveries = false
-      end
+      message.subject = "#{message.subject} | (Original recipient: #{message.to.join(',')})"
+      message.to = message.reply_to.presence || message.from
     end
   end
 end
