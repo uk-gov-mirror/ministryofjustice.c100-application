5b019e6df75c7bdc0fec3900fd173038c743b5d8
Jesus Laiz <zheileman@users.noreply.github.com>
2018-06-13 15:40:03 +0100

    Add solicitor details to C100 and C1A pdf
    We already had this sections but hardcoded to `no`. Now it will show the details if present.

--- app/presenters/summary/sections/solicitor_details.rb
+++ app/presenters/summary/sections/solicitor_details.rb
@@ -9,10 +9,36 @@ module Summary
         false
       end
 
+      # rubocop:disable Metrics/AbcSize
       def answers
+        return [
+          Answer.new(:has_solicitor, GenericYesNo::NO)
+        ] if solicitor.nil?
+
         [
-          Answer.new(:has_solicitor, GenericYesNo::NO), # Always `NO` for pilot (we are screening)
+          Answer.new(:has_solicitor, GenericYesNo::YES),
+          FreeTextAnswer.new(:solicitor_applicant_name, applicant_name),
+          FreeTextAnswer.new(:solicitor_full_name, solicitor.full_name),
+          FreeTextAnswer.new(:solicitor_firm_name, solicitor.firm_name),
+          FreeTextAnswer.new(:solicitor_address, solicitor.address),
+          FreeTextAnswer.new(:solicitor_phone_number, solicitor.phone_number),
+          FreeTextAnswer.new(:solicitor_fax_number, solicitor.fax_number),
+          FreeTextAnswer.new(:solicitor_dx_number, solicitor.dx_number),
+          FreeTextAnswer.new(:solicitor_reference, solicitor.reference),
+          FreeTextAnswer.new(:solicitor_fee_account, c100.solicitor_account_number, show: true),
+          FreeTextAnswer.new(:solicitor_email, solicitor.email),
         ].select(&:show?)
+      end
+      # rubocop:enable Metrics/AbcSize
+
+      private
+
+      def solicitor
+        @_solicitor ||= c100.solicitor
+      end
+
+      def applicant_name
+        c100.applicants.first&.full_name
       end
     end
   end
