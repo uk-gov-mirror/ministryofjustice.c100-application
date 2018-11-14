4194e182389b5d539b9ca5b3e321009b31693729
Jesus Laiz <zheileman@users.noreply.github.com>
2018-11-14 16:28:26 +0000

    Fix migration
    `find_each` doesn't perform on staging database, so doing it another way.

--- db/migrate/20181114112915_refactor_applications_audit.rb
+++ db/migrate/20181114112915_refactor_applications_audit.rb
@@ -1,12 +1,10 @@
 class RefactorApplicationsAudit < ActiveRecord::Migration[5.1]
   #
-  # This migration will not rollback as it is coupled to new code.
-  # All current stored data is preserved and nothing is lost.
+  # Current stored data is preserved and nothing is lost.
   # We just move around some fields to the new `metadata` column.
+  # Rollback will not restore to the previous state but this is on purpose.
   #
   def up
-    return if column_exists?(:completed_applications_audit, :metadata)
-
     add_column :completed_applications_audit, :metadata, :json, default: {}
 
     migrate_current_data!
@@ -15,10 +13,16 @@ class RefactorApplicationsAudit < ActiveRecord::Migration[5.1]
     remove_column :completed_applications_audit, :urgent_hearing
   end
 
+  def down
+    remove_column :completed_applications_audit, :metadata
+    add_column :completed_applications_audit, :saved, :boolean
+    add_column :completed_applications_audit, :urgent_hearing, :string
+  end
+
   private
 
   def migrate_current_data!
-    CompletedApplicationsAudit.find_each(batch_size: 100) do |record|
+    CompletedApplicationsAudit.all.each do |record|
       record.update_column(:metadata, {
         saved_for_later: record.saved,
         urgent_hearing: record.urgent_hearing,
