acbbc3dcfe0eb3494d736980762b6cac6c041dda
Jesus Laiz <zheileman@users.noreply.github.com>
2018-12-14 14:34:37 +0000

    Another attempt to Barnes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,11 @@ Rails.application.configure do
       host: uri.host, protocol: uri.scheme, port: uri.port
     }
   end
+
+  # Start Barnes to use Ruby Language Metrics on Heroku
+  # https://devcenter.heroku.com/articles/language-runtime-metrics-ruby
+  require 'barnes'
+
+  puts '> starting Barnes metrics....'
+  Barnes.start
 end
