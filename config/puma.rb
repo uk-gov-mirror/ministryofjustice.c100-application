11e35befaa56d4a3c853b4bcfc8ea64f381f3bf1
Jesus Laiz <zheileman@users.noreply.github.com>
2018-12-17 10:45:16 +0000

    More puma tweaks

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,7 @@
-require 'barnes'
-
 workers_count = ENV.fetch("WEB_CONCURRENCY") { 2 }.to_i
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 
-workers workers_count if Rails.env.production?
+workers workers_count if ENV['RAILS_ENV'] == 'production'
 threads threads_count, threads_count
 
 preload_app!
@@ -14,12 +12,11 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 on_worker_boot do
-  ActiveRecord::Base.establish_connection
-  Barnes.start
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
 end
 
 on_restart do
-  ActiveRecord::Base.connection.disconnect!
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 end
 
 before_fork do
@@ -29,10 +26,12 @@ before_fork do
     config.ram = ENV.fetch("CONTAINER_AVAILABLE_RAM") { 512 }
     config.frequency = 15 # seconds
     config.percent_usage = 0.98
+    config.reaper_status_logs = false
     config.rolling_restart_frequency = false
   end
 
   PumaWorkerKiller.start
 
+  require 'barnes'
   Barnes.start
 end
