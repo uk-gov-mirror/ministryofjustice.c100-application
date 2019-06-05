f95e52f16d6fe5be534ae668491c1c74cc6310ec
Jesus Laiz <zheileman@users.noreply.github.com>
2019-06-05 16:18:30 +0100

    Run the sanity check validation
    On the step that actually starts the application (marks it as `in progress`) we are going to do an extra validation now, to ensure the screener has been completed.
    
    Before this, there was an edge case where once you obtain a session (just visiting the postcode step) and then jumping manually to the `child_protection_cases` step, would actually let you pass with a corrupt or missing 'screening', thus failing at submission time.

--- spec/controllers/steps/miam/child_protection_cases_controller_spec.rb
+++ spec/controllers/steps/miam/child_protection_cases_controller_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Steps::Miam::ChildProtectionCasesController, type: :controller do
   it_behaves_like 'a savepoint step controller'
-  it_behaves_like 'an intermediate step controller', Steps::Miam::ChildProtectionCasesForm, C100App::MiamDecisionTree
+
+  # Following examples will stub the screener sanity check for simplicity,
+  # as the above `savepoint` examples already test the involved code.
+
+  it_behaves_like 'an intermediate step controller', Steps::Miam::ChildProtectionCasesForm, C100App::MiamDecisionTree do
+    before do
+      allow(controller).to receive(:screener_sanity_check).and_return(true)
+    end
+  end
 
   # The following shared specs can really be used in any other step controller.
   # The important thing is to be tested in at least one, as any other will behave the same.
-  it_behaves_like 'a step that can be drafted', Steps::Miam::ChildProtectionCasesForm
+  it_behaves_like 'a step that can be drafted', Steps::Miam::ChildProtectionCasesForm do
+    before do
+      allow(controller).to receive(:screener_sanity_check).and_return(true)
+    end
+  end
 end
