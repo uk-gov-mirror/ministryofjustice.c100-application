303e0080d2e251086e7c668366dd1b7d97f711a7
Jesus Laiz <zheileman@users.noreply.github.com>
2020-02-05 11:34:32 +0000

    Send daily task emails asynchronously
    The `daily_tasks` rake task run daily at night and can send quite a lot of emails, depending on how many draft applications are about to expire (reminders).
    
    Up until now we didn't have a background job queue but now we have Sidekiq and can enqueue all these emails for sending later which will make the task run faster and terminate without risk of emails not being sent.
    
    Removed the `worker.sh` script as it will be replaced with  a simpler `bin/rails daily_tasks` in the cronjob kubernetes deployment.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /usr/src/app
+cd /usr/src/app 2> /dev/null
 
 bundle exec rake db:create db:migrate
 bundle exec pumactl -F config/puma.rb start
--- worker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /usr/src/app
-
-bundle exec rake daily_tasks
