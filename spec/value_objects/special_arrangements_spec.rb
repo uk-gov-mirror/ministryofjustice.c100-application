3adee0ab3ab945861daf99290e27351053791a76
Jesus Laiz <zheileman@users.noreply.github.com>
2020-02-13 10:27:25 +0000

    Add new special arrangements DB fields and value-object
    The redesign of this step will use check boxes instead of a radio yes-no option.
    
    We will have a list of values for the check boxes and store the selected ones in an array in the DB table `court_arrangements`.
    
    This is similar to what was done in the past with the MIAM exemptions steps.

---
+++ spec/value_objects/special_arrangements_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe SpecialArrangements do
+  let(:value) { :foo }
+  subject     { described_class.new(value) }
+
+  describe '.values' do
+    it 'returns all possible values' do
+      expect(described_class.values.map(&:to_s)).to eq(%w(
+        separate_waiting_rooms
+        separate_entrance_exit
+        advance_court_viewing
+        protective_screens
+        video_link
+      ))
+    end
+  end
+end
