bacfacd232228e7a2635892c3b506f4e449dfcd7
Jesus Laiz <zheileman@users.noreply.github.com>
2020-02-17 16:57:04 +0000

    Refactor completions audit log
    This is growing too much with the addition of the special arrangements metadata.
    
    Refactor a bit the code to split the metadata to a separate helper class.
    
    Added a new metadata attribute (`arrangements`) to store a collection (array) of arrangement strings based on what options the applicant chose, either yes-no or check boxes.
    Only for new applications using the new `court_arrangements` table. Older applications will just store an empty array.

--- spec/models/completed_applications_audit_spec.rb
+++ spec/models/completed_applications_audit_spec.rb
@@ -1,29 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CompletedApplicationsAudit, type: :model do
+  let(:screener_answers_court) { instance_double(Court, name: 'Test Court') }
+
   let(:c100_application) {
     C100Application.new(
       id: '449362af-0bc3-4953-82a7-1363d479b876',
-      version: 123,
       created_at: Time.at(0),
       updated_at: Time.at(100),
-      has_solicitor: has_solicitor,
-      urgent_hearing: 'no',
-      without_notice: 'yes',
       submission_type: 'online',
-      payment_type: 'cash',
-      declaration_signee_capacity: 'applicant',
     )
   }
 
-  let(:user_id) { nil }
-  let(:has_solicitor) { 'yes' }
-  let(:screener_answers_court) { instance_double(Court, name: 'Test Court') }
-  let(:screener_answers) { instance_double(ScreenerAnswers, children_postcodes: 'abcd 123') }
-
   before do
-    allow(c100_application).to receive(:user_id).and_return(user_id)
-    allow(c100_application).to receive(:screener_answers).and_return(screener_answers)
     allow(c100_application).to receive(:screener_answers_court).and_return(screener_answers_court)
   end
 
@@ -36,6 +25,13 @@ RSpec.describe CompletedApplicationsAudit, type: :model do
   end
 
   describe '.log!' do
+    # Metadata tested separately in the `AuditHelper` spec
+    let(:audit_helper) { instance_double(AuditHelper, metadata: { foo: 'bar' }) }
+
+    before do
+      allow(AuditHelper).to receive(:new).with(c100_application).and_return(audit_helper)
+    end
+
     it 'creates a record with the expected information' do
       expect(
         described_class
@@ -45,49 +41,10 @@ RSpec.describe CompletedApplicationsAudit, type: :model do
         reference_code: '1970/01/449362AF',
         submission_type: 'online',
         court: 'Test Court',
-        metadata: {
-          v: 123,
-          postcode: 'ABCD1**',
-          c1a_form: false,
-          c8_form: false,
-          saved_for_later: false,
-          legal_representation: 'yes',
-          urgent_hearing: 'no',
-          without_notice: 'yes',
-          payment_type: 'cash',
-          signee_capacity: 'applicant',
-        }
+        metadata: { foo: 'bar' },
       )
 
       described_class.log!(c100_application)
-    end
-
-    context 'for an application with `has_solicitor` attribute set to `nil`' do
-      let(:has_solicitor) { nil }
-
-      it 'defaults to `no`' do
-        expect(
-          described_class
-        ).to receive(:create).with(
-          hash_including(metadata: hash_including(legal_representation: 'no'))
-        )
-
-        described_class.log!(c100_application)
-      end
-    end
-
-    context 'for a saved application' do
-      let(:user_id) { 123 }
-
-      it 'creates a record with the expected information' do
-        expect(
-          described_class
-        ).to receive(:create).with(
-          hash_including(metadata: hash_including(saved_for_later: true))
-        )
-
-        described_class.log!(c100_application)
-      end
     end
   end
 
