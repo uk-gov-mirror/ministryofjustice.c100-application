4ae3c088308f2796e9226d4667d618fe4b8e24ee
Jesus Laiz <zheileman@users.noreply.github.com>
2020-02-20 13:34:15 +0000

    Update and simplify filter_parameters
    Rails consider the words as wildcards so no need to specify all the possible combinations, we can simplify the parameter list a lot.
    
    Note: Sentry will not interpret the parameters as regex but instead as whole words with boundaries, so in order to force it to treat them as regex, we introduce a "+" (repetition metacharacter) in each parameter name, and that will do the job.

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,6 +3,9 @@
 Raven.configure do |config|
   config.environments = %w( production )
   config.silence_ready = true
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+  # Convert to regex, otherwise Sentry will threat them as words
+  # https://github.com/getsentry/raven-ruby/blob/master/lib/raven/processor/sanitizedata.rb
+  config.sanitize_fields = Rails.application.config.filter_parameters.map { |w| "#{w}+" }
 end
 # :nocov:
