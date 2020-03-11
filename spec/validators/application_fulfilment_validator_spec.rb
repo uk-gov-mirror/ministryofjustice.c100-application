1203b74a265c9fcf1ce545bdd1345a94bda5850d
Jesus Laiz <zheileman@users.noreply.github.com>
2020-03-11 15:16:02 +0000

    Add validation for children and respondents
    Same as we did with applicants, we add validation for having at least one child and at least one respondent.
    
    For now this will be all. We can decide to include more validations later on.
    
    Reworked a bit the loop to retrieve the error details so it works when there is more than one, like we have now.
    
    The final little PR after this one will be to enable this for all users (without feature-flag) to be able to release to production.

--- spec/validators/application_fulfilment_validator_spec.rb
+++ spec/validators/application_fulfilment_validator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 module Test
-  C100ApplicationValidatable = Struct.new(:screener_answers, :applicants, keyword_init: true) do
+  C100ApplicationValidatable = Struct.new(:screener_answers, :children, :applicants, :respondents, keyword_init: true) do
     include ActiveModel::Validations
     validates_with ApplicationFulfilmentValidator
   end
@@ -10,10 +10,18 @@ end
 RSpec.describe ApplicationFulfilmentValidator, type: :model do
   subject { Test::C100ApplicationValidatable.new(arguments) }
 
+  let(:arguments) { { children: children, applicants: applicants, respondents: respondents } }
+
+  let(:children)    { [Object] }
+  let(:applicants)  { [Object] }
+  let(:respondents) { [Object] }
+
   # TODO: feature-flag code, to be remove once released
   context 'validations are run based on feature flag' do
     let(:screener_answers) { instance_double(ScreenerAnswers, local_court: local_court) }
-    let(:arguments) { { applicants: [] } }
+
+    # make it fail by not having children
+    let(:children) { [] }
 
     before do
       allow(subject).to receive(:screener_answers).and_return(screener_answers)
@@ -36,29 +44,67 @@ RSpec.describe ApplicationFulfilmentValidator, type: :model do
     end
   end
 
-  describe 'applicants validation' do
+  context 'individual validations' do
     # TODO: feature-flag code, to be remove once released
     # Assume in following tests we must always validate
     before do
       allow_any_instance_of(described_class).to receive(:must_validate?).and_return(true)
     end
 
-    context 'there are no applicants' do
-      let(:arguments) { { applicants: [] } }
+    context 'children' do
+      context 'there is at least one child' do
+        it 'is valid' do
+          subject.valid?
+          expect(subject.errors.details.include?(:children)).to eq(false)
+        end
+      end
 
-      it 'is invalid' do
-        expect(subject).not_to be_valid
-        expect(subject.errors.details[:applicants][0][:error]).to eq(:blank)
-        expect(subject.errors.details[:applicants][0][:change_path]).to eq('/steps/applicant/names/')
+      context 'there are no children' do
+        let(:children) { [] }
+
+        it 'is invalid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.details[:children][0][:error]).to eq(:blank)
+          expect(subject.errors.details[:children][0][:change_path]).to eq('/steps/children/names/')
+        end
       end
     end
 
-    context 'there is at least one applicant' do
-      let(:arguments) { { applicants: [Object] } }
+    context 'applicants' do
+      context 'there is at least one applicant' do
+        it 'is valid' do
+          subject.valid?
+          expect(subject.errors.details.include?(:applicants)).to eq(false)
+        end
+      end
 
-      it 'is valid' do
-        subject.valid?
-        expect(subject.errors.details.include?(:applicants)).to eq(false)
+      context 'there are no applicants' do
+        let(:applicants) { [] }
+
+        it 'is invalid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.details[:applicants][0][:error]).to eq(:blank)
+          expect(subject.errors.details[:applicants][0][:change_path]).to eq('/steps/applicant/names/')
+        end
+      end
+    end
+
+    context 'respondents' do
+      context 'there is at least one respondent' do
+        it 'is valid' do
+          subject.valid?
+          expect(subject.errors.details.include?(:respondents)).to eq(false)
+        end
+      end
+
+      context 'there are no respondents' do
+        let(:respondents) { [] }
+
+        it 'is invalid' do
+          expect(subject).not_to be_valid
+          expect(subject.errors.details[:respondents][0][:error]).to eq(:blank)
+          expect(subject.errors.details[:respondents][0][:change_path]).to eq('/steps/respondent/names/')
+        end
       end
     end
   end
