746d5ae6efde84c9c72fb5114cc55060bf3ba10c
Jesus Laiz <zheileman@users.noreply.github.com>
2020-03-31 11:30:58 +0100

    Remove old language step

--- spec/controllers/steps/application/language_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Steps::Application::LanguageController, type: :controller do
-  it_behaves_like 'an intermediate step controller', Steps::Application::LanguageForm, C100App::ApplicationDecisionTree
-end
