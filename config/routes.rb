8d7d31ed9694e12c688b5ad41bf8ce4fffd9b31b
Jesus Laiz <zheileman@users.noreply.github.com>
2020-03-31 12:00:47 +0100

    Remove old special arrangements steps

--- config/routes.rb
+++ config/routes.rb
@@ -130,8 +130,6 @@ Rails.application.routes.draw do
       edit_step :details
       edit_step :litigation_capacity
       edit_step :litigation_capacity_details
-      edit_step :special_arrangements
-      edit_step :special_assistance
       edit_step :payment
       edit_step :submission
       show_step :receipt_email_check
--- spec/controllers/steps/application/special_arrangements_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Steps::Application::SpecialArrangementsController, type: :controller do
-  it_behaves_like 'an intermediate step controller', Steps::Application::SpecialArrangementsForm, C100App::ApplicationDecisionTree
-end
