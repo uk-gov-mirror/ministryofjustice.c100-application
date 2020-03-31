67ad60015c0a10f4fa0c808d73c4d502c3917aa5
Jesus Laiz <zheileman@users.noreply.github.com>
2020-03-31 11:39:40 +0100

    Remove old intermediary step

--- spec/controllers/steps/application/intermediary_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Steps::Application::IntermediaryController, type: :controller do
-  it_behaves_like 'an intermediate step controller', Steps::Application::IntermediaryForm, C100App::ApplicationDecisionTree
-end
