bb3778a9d4534baf6e5a668964d49ba6996cb6ce
Jesus Laiz <zheileman@users.noreply.github.com>
2020-04-16 14:57:15 +0100

    Migrate fourth page of MIAM exemptions (ADR)
    After running this through our service designer, we've simplified the page so instead of 2 check boxes revealing 3 and 2 each more, now it is just a plain list of check boxes, identical to the paper form.
    
    This let's us remove some of the ugly hint text with bullet points, and make the page simpler to read.

--- spec/forms/steps/miam_exemptions/adr_form_spec.rb
+++ spec/forms/steps/miam_exemptions/adr_form_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 RSpec.describe Steps::MiamExemptions::AdrForm do
   let(:arguments) { {
     c100_application: c100_application,
-    previous_attendance: '1',
-    previous_exemption: '0',
+    adr: ['previous_attendance'],
   } }
 
   let(:c100_application) { instance_double(C100Application, miam_exemption: miam_exemption_record) }
@@ -12,13 +11,9 @@ RSpec.describe Steps::MiamExemptions::AdrForm do
 
   subject { described_class.new(arguments) }
 
-  describe 'custom getters override' do
-    it 'returns true if the exemption is in the list' do
-      expect(subject.previous_attendance).to eq(true)
-    end
-
-    it 'returns false if the exemption is not in the list' do
-      expect(subject.previous_exemption).to eq(false)
+  describe 'custom getter override' do
+    it 'returns all the exemptions in all attributes' do
+      expect(subject.exemptions_collection).to eq(['previous_attendance'])
     end
   end
 
@@ -28,16 +23,6 @@ RSpec.describe Steps::MiamExemptions::AdrForm do
 
       it 'raises an error' do
         expect { subject.save }.to raise_error(BaseForm::C100ApplicationNotFound)
-      end
-    end
-
-    context 'when form is valid' do
-      it 'saves the record' do
-        expect(miam_exemption_record).to receive(:update).with(
-          adr: [:previous_attendance],
-        ).and_return(true)
-
-        expect(subject.save).to be(true)
       end
     end
 
@@ -53,7 +38,36 @@ RSpec.describe Steps::MiamExemptions::AdrForm do
 
       it 'has a validation error' do
         expect(subject).to_not be_valid
-        expect(subject.errors[:adr_none]).to_not be_empty
+        expect(subject.errors.added?(:adr, :blank)).to eq(true)
+      end
+    end
+
+    context 'when invalid checkbox values are submitted' do
+      context 'in `adr` attribute' do
+        let(:arguments) { {
+          c100_application: c100_application,
+          adr: ['foobar'],
+        } }
+
+        it 'does not save the record' do
+          expect(miam_exemption_record).not_to receive(:update)
+          expect(subject.save).to be(false)
+        end
+
+        it 'has a validation error' do
+          expect(subject).to_not be_valid
+          expect(subject.errors.added?(:adr, :blank)).to eq(true)
+        end
+      end
+    end
+
+    context 'when form is valid' do
+      it 'saves the record' do
+        expect(miam_exemption_record).to receive(:update).with(
+          adr: %w(previous_attendance),
+        ).and_return(true)
+
+        expect(subject.save).to be(true)
       end
     end
   end
