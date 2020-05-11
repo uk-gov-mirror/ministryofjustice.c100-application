6c73a6ed6baf1fde1de7906e43761ee10e02d3fe
Jesus Laiz <zheileman@users.noreply.github.com>
2020-05-11 10:48:51 +0100

    Add international resident details text area
    Add the DB field and update the form object and view to show this text area when selecting the YES radio option.
    
    It follows the same pattern as the rest of the international questions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_112122) do
+ActiveRecord::Schema.define(version: 2020_05_11_091814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_112122) do
     t.integer "version", default: 5
     t.string "declaration_signee"
     t.string "declaration_signee_capacity"
+    t.text "international_resident_details"
     t.index ["status"], name: "index_c100_applications_on_status"
     t.index ["user_id"], name: "index_c100_applications_on_user_id"
   end
