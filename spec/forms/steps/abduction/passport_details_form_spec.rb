e5088f4f828690e807b23af29e6a440ed741f142
Jesus Laiz <zheileman@users.noreply.github.com>
2020-05-15 12:26:10 +0100

    Refactor passport possession check boxes
    This was a leftover from the old framework and way of handling check box stuff.
    
    Now that the whole service is migrated we can also cleanup this and make these check boxes behave more like the ones in the rest of the service, and we do not need a custom attribute class anymore.

--- spec/forms/steps/abduction/passport_details_form_spec.rb
+++ spec/forms/steps/abduction/passport_details_form_spec.rb
@@ -4,15 +4,13 @@ RSpec.describe Steps::Abduction::PassportDetailsForm do
   let(:arguments) { {
     c100_application: c100_application,
     children_multiple_passports: 'no',
-    passport_possession_mother: true,
-    passport_possession_father: false,
-    passport_possession_other: passport_possession_other,
+    passport_possession: passport_possession,
     passport_possession_other_details: passport_possession_other_details
   } }
 
   let(:c100_application) { instance_double(C100Application) }
 
-  let(:passport_possession_other) { false }
+  let(:passport_possession) { ['mother'] }
   let(:passport_possession_other_details) { nil }
 
   subject { described_class.new(arguments) }
@@ -21,45 +19,51 @@ RSpec.describe Steps::Abduction::PassportDetailsForm do
     context 'validations' do
       it { should validate_presence_of(:children_multiple_passports, :inclusion) }
 
-      context 'when `passport_possession_other_details` is checked' do
-        let(:passport_possession_other) { true }
+      context 'when passport possession `other` is checked' do
+        let(:passport_possession) { ['other'] }
         it { should validate_presence_of(:passport_possession_other_details) }
       end
 
-      context 'when `passport_possession_other_details` is not checked' do
-        let(:passport_possession_other) { false }
+      context 'when passport possession `other` is not checked' do
         it { should_not validate_presence_of(:passport_possession_other_details) }
       end
     end
 
-    context 'when `passport_possession_other` is true and `passport_possession_other_details` is filled' do
-      let(:passport_possession_other) { true }
+    context 'when passport possession `other` is true and `passport_possession_other_details` is filled' do
+      let(:passport_possession) { %w(mother other) }
       let(:passport_possession_other_details) { 'blah blah' }
 
       it_behaves_like 'a has-one-association form',
                       association_name: :abduction_detail,
                       expected_attributes: {
-                          children_multiple_passports: GenericYesNo::NO,
-                          passport_possession_mother: true,
-                          passport_possession_father: false,
-                          passport_possession_other: true,
-                          passport_possession_other_details: 'blah blah'
+                        children_multiple_passports: GenericYesNo::NO,
+                        passport_possession: %w(mother other),
+                        passport_possession_other_details: 'blah blah'
+                      }
+    end
+
+    context 'filters out invalid options (tampering)' do
+      let(:passport_possession) { %w(father foobar) }
+
+      it_behaves_like 'a has-one-association form',
+                      association_name: :abduction_detail,
+                      expected_attributes: {
+                        children_multiple_passports: GenericYesNo::NO,
+                        passport_possession: ['father'],
+                        passport_possession_other_details: nil
                       }
     end
 
     # Mutant killer
-    context 'when `passport_possession_other` is false and `passport_possession_other_details` is filled' do
-      let(:passport_possession_other) { false }
+    context 'when passport possession `other` is false and `passport_possession_other_details` is filled' do
       let(:passport_possession_other_details) { 'blah blah' }
 
       it_behaves_like 'a has-one-association form',
                       association_name: :abduction_detail,
                       expected_attributes: {
-                          children_multiple_passports: GenericYesNo::NO,
-                          passport_possession_mother: true,
-                          passport_possession_father: false,
-                          passport_possession_other: false,
-                          passport_possession_other_details: nil
+                        children_multiple_passports: GenericYesNo::NO,
+                        passport_possession: ['mother'],
+                        passport_possession_other_details: nil
                       }
     end
   end
