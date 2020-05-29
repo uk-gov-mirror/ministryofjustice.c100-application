021d16d61679694c34d9de5d1f2453800130c52b
Jesus Laiz <zheileman@users.noreply.github.com>
2020-05-29 15:19:44 +0100

    Refactor back office emails with form object
    Use the form object to store and validate the submitted form data, and provide user-friendly errors to the view, as we do in the rest of the application.
    
    Improved the email submission search, by filtering properly when an email is entered, in addition to the reference code.
    
    So for example if searching for reference code XYZ, we find results, but an email is also used in the search and none of the results match the email, then we don't show results.

--- app/controllers/backoffice/emails_controller.rb
+++ app/controllers/backoffice/emails_controller.rb
@@ -3,19 +3,22 @@ module Backoffice
     include Auth0Secured
 
     def index
+      @form = Backoffice::LookupForm.new
       @report = Reports::FailedEmailsReport.list
     end
 
     def lookup
-      @reference_code = params[:reference_code]&.strip
-      @email_address  = params[:email_address]&.strip
+      @form = Backoffice::LookupForm.new(form_params)
+      @report = []
 
-      @report = EmailSubmissionsAudit.find_records(@reference_code, @email_address)
-      @submission = C100Application.find_by_reference_code(@reference_code)&.email_submission
+      render :lookup && return if @form.invalid?
+
+      @report = EmailSubmissionsAudit.find_records(@form.reference_code, @form.email_address)
+      @submission = email_submission_details(@form.reference_code, @form.email_address)
 
       audit!(
         action: :email_lookup,
-        details: { reference_code: @reference_code, email_address: @email_address, found: @report.size }
+        details: { reference_code: @form.reference_code, email_address: @form.email_address, found: @report.size }
       )
     end
 
@@ -32,6 +35,24 @@ module Backoffice
     end
 
     private
+
+    def form_params
+      params.require(:backoffice_lookup_form).permit(
+        :reference_code,
+        :email_address,
+      )
+    end
+
+    def email_submission_details(reference, email = nil)
+      email_submission = C100Application.find_by_reference_code(reference).try(:email_submission)
+      return if email_submission.nil?
+
+      email_submission if [
+        email_submission.to_address,
+        email_submission.email_copy_to,
+        nil
+      ].include?(email.presence)
+    end
 
     def audit_resend(email, resend_type:)
       details = {
