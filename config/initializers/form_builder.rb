550b67b348c37736ea6ae0c7133ba6fb0d86a6fd
Jesus Laiz <zheileman@users.noreply.github.com>
2020-06-29 08:56:50 +0100

    Bump form builder and refactor captions
    Following the update of the `govuk_design_system_formbuilder` gem with support for captions, we can now refactor most of the places where we were using these manually, to use instead the form builder and locales.
    
    This have the advantage of the caption being part of the page header h1 (which in itself is part of the fieldset in many of these pages).
    Also remove a visual glitch when there are errors, where the caption will not be included in the error block.

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -1,8 +1,9 @@
 ActionView::Base.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
 
 GOVUKDesignSystemFormBuilder.configure do |config|
-  config.default_legend_tag  = 'h1'
-  config.default_legend_size = 'xl'
+  config.default_legend_tag   = 'h1'
+  config.default_legend_size  = 'xl'
+  config.default_caption_size = 'xl'
 end
 
 GOVUKDesignSystemFormBuilder::FormBuilder.class_eval do
