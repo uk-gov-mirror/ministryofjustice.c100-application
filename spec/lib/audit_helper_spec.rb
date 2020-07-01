33fb91540bf6c6e14877ef39deb167bf41b58946
Jesus Laiz <zheileman@users.noreply.github.com>
2020-07-01 10:44:27 +0100

    Add online payment details to back office
    Added the gbs code and the payment ID to the audit table when an application is completed, if paid online.
    
    Exposed this information in the back office as might be helpful to diagnose problems in the future.

--- spec/lib/audit_helper_spec.rb
+++ spec/lib/audit_helper_spec.rb
@@ -8,21 +8,23 @@ describe AuditHelper do
       reduced_litigation_capacity: 'yes',
       urgent_hearing: 'no',
       without_notice: 'yes',
-      payment_type: 'cash',
       declaration_signee_capacity: 'applicant',
     )
   }
 
   let(:user_id) { nil }
-  let(:screener_answers_court) { instance_double(Court, name: 'Test Court') }
-  let(:screener_answers) { instance_double(ScreenerAnswers, children_postcodes: 'abcd 123') }
-
+  let(:payment_type) { 'cash' }
   let(:court_arrangement) { nil }
+
+  let(:screener_answers_court) { instance_double(Court, name: 'Test Court', gbs: 'X123') }
+  let(:screener_answers) { instance_double(ScreenerAnswers, children_postcodes: 'abcd 123') }
 
   subject { described_class.new(c100_application) }
 
   before do
     allow(c100_application).to receive(:user_id).and_return(user_id)
+    allow(c100_application).to receive(:payment_type).and_return(payment_type)
+
     allow(c100_application).to receive(:screener_answers).and_return(screener_answers)
     allow(c100_application).to receive(:screener_answers_court).and_return(screener_answers_court)
     allow(c100_application).to receive(:court_arrangement).and_return(court_arrangement)
@@ -46,10 +48,10 @@ describe AuditHelper do
         payment_type: 'cash',
         signee_capacity: 'applicant',
         arrangements: [],
+        payment_details: {},
       )
     end
 
-    # TODO we can cleanup this when all applications are using the new table
     context 'when we have court arrangements' do
       let(:court_arrangement) {
         CourtArrangement.new(
@@ -70,6 +72,19 @@ describe AuditHelper do
           protective_screens
           hearing_loop
         ))
+      end
+    end
+
+    context 'when payment is online' do
+      let(:payment_type) { 'online' }
+      let(:payment_intent) { instance_double(PaymentIntent, payment_id: 'foobar') }
+
+      it 'returns the expected information' do
+        expect(c100_application).to receive(:payment_intent).and_return(payment_intent)
+
+        expect(
+          subject.metadata[:payment_details]
+        ).to eq({ gbs_code: 'X123', payment_id: 'foobar' })
       end
     end
 
