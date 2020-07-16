828390ab747f3490abd406cded370d57b90fbc8c
Jesus Laiz <zheileman@users.noreply.github.com>
2020-07-16 16:07:27 +0100

    Update ConsentOrderForm reset functionality
    We need to do the same we are doing on `ChildProtectionCasesForm` because if there is a consent order, then the application will bypass the MIAM steps.
    
    We do this as a safety measure in case the user previously said NO to the consent order and entered MIAM details, but then, through the check your answers perhaps, they change their answer from NO to YES, which should reset the previously entered MIAM details.

--- app/forms/steps/miam/child_protection_cases_form.rb
+++ app/forms/steps/miam/child_protection_cases_form.rb
@@ -3,6 +3,10 @@ module Steps
     class ChildProtectionCasesForm < BaseForm
       include SingleQuestionForm
 
+      # Child emergency proceedings are automatically exempt from going to MIAM and thus,
+      # if for whatever reason the user entered any MIAM details, we should
+      # preemptively reset them if they enable (answer YES) child_protection_cases.
+      #
       yes_no_attribute :child_protection_cases, reset_when_yes: [
         :miam_acknowledgement,
         :miam_attended,
--- app/forms/steps/miam/consent_order_form.rb
+++ app/forms/steps/miam/consent_order_form.rb
@@ -3,7 +3,19 @@ module Steps
     class ConsentOrderForm < BaseForm
       include SingleQuestionForm
 
-      yes_no_attribute :consent_order
+      # Consent orders are automatically exempt from going to MIAM and thus,
+      # if for whatever reason the user entered any MIAM details, we should
+      # preemptively reset them if they enable (answer YES) consent order.
+      #
+      yes_no_attribute :consent_order, reset_when_yes: [
+        :child_protection_cases,
+        :miam_acknowledgement,
+        :miam_attended,
+        :miam_exemption_claim,
+        :miam_certification,
+        Steps::Miam::CertificationDateForm,
+        Steps::Miam::CertificationDetailsForm,
+      ]
     end
   end
 end
