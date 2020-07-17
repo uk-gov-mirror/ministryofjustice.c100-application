c87bd83e5df2cea339c9373eeeffd9fadbbcb6ea
Jesus Laiz <zheileman@users.noreply.github.com>
2020-07-17 12:27:38 +0100

    Add a couple more of inquiry methods
    These will be used later on as we need to query the current application to know if it is a consent order in order to show/hide some copy in the confirmation page as well as to run different branches of the decision trees.

--- app/models/concerns/application_inquiry_methods.rb
+++ app/models/concerns/application_inquiry_methods.rb
@@ -1,4 +1,6 @@
 module ApplicationInquiryMethods
+  YES_ANSWER = GenericYesNo::YES.to_s
+
   def online_submission?
     submission_type.eql?(SubmissionType::ONLINE.to_s)
   end
@@ -7,12 +9,20 @@ module ApplicationInquiryMethods
     payment_type.eql?(PaymentType::ONLINE.to_s)
   end
 
+  def consent_order?
+    consent_order.eql?(YES_ANSWER)
+  end
+
+  def child_protection_cases?
+    child_protection_cases.eql?(YES_ANSWER)
+  end
+
   def confidentiality_enabled?
-    address_confidentiality.eql?(GenericYesNo::YES.to_s)
+    address_confidentiality.eql?(YES_ANSWER)
   end
 
   def has_solicitor?
-    has_solicitor.eql?(GenericYesNo::YES.to_s)
+    has_solicitor.eql?(YES_ANSWER)
   end
 
   def has_safety_concerns?
@@ -22,6 +32,6 @@ module ApplicationInquiryMethods
       children_abuse,
       substance_abuse,
       other_abuse
-    ].any? { |concern| concern.eql?(GenericYesNo::YES.to_s) }
+    ].any? { |concern| concern.eql?(YES_ANSWER) }
   end
 end
