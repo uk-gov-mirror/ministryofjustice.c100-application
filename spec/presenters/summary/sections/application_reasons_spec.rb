2ec226b2f5b1350f062679438bce3f09736bb1e0
Jesus Laiz <zheileman@users.noreply.github.com>
2020-08-21 16:13:51 +0100

    Add permission details to the PDF
    Ticket: https://mojdigital.teamwork.com/#/tasks/21392586
    Parent ticket: https://mojdigital.teamwork.com/#/tasks/21392255
    
    Add to the PDF the details of the permission. By default, permission is not required, or just `No`, as it has been all this time because we didn't have non-parents.
    
    From now on, we also need to consider the possibility of some applications seeking permission to apply.
    
    There are a couple places in the PDF where this is relevant.

--- spec/presenters/summary/sections/application_reasons_spec.rb
+++ spec/presenters/summary/sections/application_reasons_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 module Summary
   describe Sections::ApplicationReasons do
     let(:c100_application) {
-      instance_double(C100Application,
-        application_details: 'details',
-    ) }
+      instance_double(
+        C100Application,
+        permission_sought: permission_sought,
+        permission_details: permission_details,
+        application_details: application_details,
+      )
+    }
+
+    let(:permission_sought) { nil }
+    let(:permission_details) { nil }
+    let(:application_details) { 'application details' }
 
     subject { described_class.new(c100_application) }
 
@@ -20,16 +28,55 @@ module Summary
     end
 
     describe '#answers' do
-      it 'has the correct rows' do
-        expect(answers.count).to eq(2)
+      context 'when permission details are `nil` (permission not required)' do
+        it 'has the correct rows' do
+          expect(answers.count).to eq(2)
 
-        expect(answers[0]).to be_an_instance_of(Answer)
-        expect(answers[0].question).to eq(:applied_for_permission)
-        expect(answers[0].value).to eq(GenericYesNo::NO)
+          expect(answers[0]).to be_an_instance_of(Answer)
+          expect(answers[0].question).to eq(:permission_sought)
+          expect(answers[0].value).to eq(:not_required)
 
-        expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[1].question).to eq(:application_details)
-        expect(c100_application).to have_received(:application_details)
+          expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[1].question).to eq(:application_details)
+          expect(answers[1].value).to eq(application_details)
+        end
+      end
+
+      context 'when permission sought is `yes`' do
+        let(:permission_sought) { 'yes' }
+
+        it 'has the correct rows' do
+          expect(answers.count).to eq(2)
+
+          expect(answers[0]).to be_an_instance_of(Answer)
+          expect(answers[0].question).to eq(:permission_sought)
+          expect(answers[0].value).to eq(permission_sought)
+
+          expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[1].question).to eq(:application_details)
+          expect(answers[1].value).to eq(application_details)
+        end
+      end
+
+      context 'when permission sought is `no`' do
+        let(:permission_sought) { 'no' }
+        let(:permission_details) { 'permission details' }
+
+        it 'has the correct rows' do
+          expect(answers.count).to eq(3)
+
+          expect(answers[0]).to be_an_instance_of(Answer)
+          expect(answers[0].question).to eq(:permission_sought)
+          expect(answers[0].value).to eq(permission_sought)
+
+          expect(answers[1]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[1].question).to eq(:permission_details)
+          expect(answers[1].value).to eq(permission_details)
+
+          expect(answers[2]).to be_an_instance_of(FreeTextAnswer)
+          expect(answers[2].question).to eq(:application_details)
+          expect(answers[2].value).to eq(application_details)
+        end
       end
     end
   end
