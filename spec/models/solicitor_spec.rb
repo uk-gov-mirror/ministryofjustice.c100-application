20e15a2a0260592dcd7e340991e682fcdca140be
Jesus Laiz <zheileman@users.noreply.github.com>
2021-01-08 10:51:17 +0000

    Cleanup of unused solicitor address DB field
    We don't need this anymore as we've migrated the address to structured data.
    No applications are using this DB field anymore.
    
    Removal of the attribute and unused code.

--- spec/models/solicitor_spec.rb
+++ spec/models/solicitor_spec.rb
@@ -2,29 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Solicitor, type: :model do
   it_behaves_like 'a model with structured address details'
-
-  # TODO: we need to maintain backwards compatibility for some time
-  describe '#full_address' do
-    subject { described_class.new(address: address_string, address_data: address_hash) }
-
-    let(:address_string) { 'address' }
-    let(:address_hash) do
-      {
-        address_line_1: 'address_line_1',
-        address_line_2: '',
-        town: 'town',
-        country: 'country',
-        postcode: 'postcode'
-      }
-    end
-
-    context 'when we have old `address` but not new `address_data`' do
-      let(:address_hash) { {} }
-      it { expect(subject.full_address).to eq('address') }
-    end
-
-    context 'when we have old `address` and also new `address_data`' do
-      it { expect(subject.full_address).to eq('address_line_1, town, country, postcode') }
-    end
-  end
 end
