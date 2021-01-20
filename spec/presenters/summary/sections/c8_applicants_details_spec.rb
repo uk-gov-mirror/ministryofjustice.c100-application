ab6326b4a8252c22f49331e15d9cd761ddae02de
Jesus Laiz <zheileman@users.noreply.github.com>
2021-01-20 09:53:48 +0000

    Reposition C8 previous addresses section
    Ticket: https://trello.com/c/tqvItimB
    
    A request from HMCTS came in where some courts where having issues
    with the positioning of the `previous addresses` (if applicant
    entered any) in the C8 (when the address confidentiality is enabled).
    
    A suggestion was made to reposition the previous addresses section
    towards the bottom of the applicant contact details, separated from
    the current address.

--- spec/presenters/summary/sections/c8_applicants_details_spec.rb
+++ spec/presenters/summary/sections/c8_applicants_details_spec.rb
@@ -49,7 +49,7 @@ module Summary
 
     describe '#answers' do
       it 'has the correct number of rows' do
-        expect(answers.count).to eq(9)
+        expect(answers.count).to eq(10)
       end
 
       it 'has the correct rows in the right order' do
@@ -66,27 +66,30 @@ module Summary
         expect(answers[2].value).to eq('full address')
 
         expect(answers[3]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[3].question).to eq(:person_residence_history)
-        expect(answers[3].value).to eq('history')
+        expect(answers[3].question).to eq(:person_email)
+        expect(answers[3].value).to eq('email')
 
         expect(answers[4]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[4].question).to eq(:person_email)
-        expect(answers[4].value).to eq('email')
+        expect(answers[4].question).to eq(:person_home_phone)
+        expect(answers[4].value).to eq('home_phone')
 
         expect(answers[5]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[5].question).to eq(:person_home_phone)
-        expect(answers[5].value).to eq('home_phone')
+        expect(answers[5].question).to eq(:person_mobile_phone)
+        expect(answers[5].value).to eq('mobile_phone')
 
-        expect(answers[6]).to be_an_instance_of(FreeTextAnswer)
-        expect(answers[6].question).to eq(:person_mobile_phone)
-        expect(answers[6].value).to eq('mobile_phone')
+        expect(answers[6]).to be_an_instance_of(Answer)
+        expect(answers[6].question).to eq(:person_voicemail_consent)
+        expect(answers[6].value).to eq('yes')
 
-        expect(answers[7]).to be_an_instance_of(Answer)
-        expect(answers[7].question).to eq(:person_voicemail_consent)
-        expect(answers[7].value).to eq('yes')
+        expect(answers[7]).to be_an_instance_of(Partial)
+        expect(answers[7].name).to eq(:row_blank_space)
 
-        expect(answers[8]).to be_an_instance_of(Partial)
-        expect(answers[8].name).to eq(:row_blank_space)
+        expect(answers[8]).to be_an_instance_of(FreeTextAnswer)
+        expect(answers[8].question).to eq(:person_residence_history)
+        expect(answers[8].value).to eq('history')
+
+        expect(answers[9]).to be_an_instance_of(Partial)
+        expect(answers[9].name).to eq(:row_blank_space)
       end
     end
   end
