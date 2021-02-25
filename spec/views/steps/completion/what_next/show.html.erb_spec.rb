96e6404dd014e3209766ce81d1de51289a5e7d2a
Jesus Laiz <zheileman@users.noreply.github.com>
2021-02-25 16:12:47 +0000

    Update copy in online submissions confirmation page
    Some copy changes are needed, only in the online submission confirmation page, to indicate the user where to send documents, as centralised courts will have a different email address (and postal address).
    
    Essentially, added a new block to the page to show this information.

--- spec/views/steps/completion/what_next/show.html.erb_spec.rb
+++ spec/views/steps/completion/what_next/show.html.erb_spec.rb
@@ -2,38 +2,55 @@ require 'rails_helper'
 
 RSpec.describe 'steps/completion/what_next/show', type: :view do
   let(:consent_order) { GenericYesNo::YES }
+  let(:under_age) { true }
+  let(:applicants_collection_double) { double(under_age?: under_age) }
   let(:c100_application) { C100Application.new(id: '449362af-0bc3-4953-82a7-1363d479b876', created_at: Time.at(0), consent_order: consent_order) }
 
   before do
+    allow(c100_application).to receive(:applicants).and_return(applicants_collection_double)
+
     assign(:c100_application, c100_application)
 
     assign(
-        :court,
-        double('Court',
-               name: 'Test court',
-               email: 'court@example.com',
-               slug: 'test-court',
-               full_address: ['10', 'downing', 'st']
-        )
+      :court,
+      double('Court',
+        name: 'Test court',
+        email: 'court@example.com',
+        slug: 'test-court',
+        full_address: ['10', 'downing', 'st']
+      )
     )
 
     render
   end
 
-  context 'when application is a consent order' do
-    it 'should render `with_consent_order` partial' do
-      expect(view).to_not render_template('steps/completion/what_next/_without_consent_order')
-      expect(view).to render_template('steps/completion/what_next/_with_consent_order')
+  context 'consent order' do
+    it 'should render the `consent_order_section` partial' do
+      expect(view).to render_template('steps/completion/what_next/_consent_order_section')
     end
   end
 
-  context 'when application is not a consent order' do
-    let(:consent_order) { GenericYesNo::NO }
+  context 'litigation friend' do
+    context 'when applicant is under age' do
+      let(:under_age) { true }
 
-    it 'should render `without_consent_order` partial' do
-      expect(view).to_not render_template('steps/completion/what_next/_with_consent_order')
-      expect(view).to render_template('steps/completion/what_next/_without_consent_order')
+      it 'should render the `under_age_section` partial' do
+        expect(view).to render_template('steps/completion/what_next/_under_age_section')
+      end
+    end
+
+    context 'when applicant is not under age' do
+      let(:under_age) { false }
+
+      it 'should not render the `under_age_section` partial' do
+        expect(view).to_not render_template('steps/completion/what_next/_under_age_section')
+      end
     end
   end
 
+  context 'court documents section' do
+    it 'should not render the `court_documents` partial' do
+      expect(view).to_not render_template('steps/shared/_court_documents')
+    end
+  end
 end
