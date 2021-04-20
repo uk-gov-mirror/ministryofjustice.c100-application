c919870bc69fb477bed0b98dbfe89a45d36b2e26
Gary Taylor <gary.taylor@hismessages.com>
2021-04-20 07:15:58 +0100

    RST-3292 Cookie banner tidy up

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,5 +1,6 @@
 class CookiesController < ApplicationController
   include CookiesHelper
+  include ApplicationHelper
 
   def update(cookie_expiry: Rails.application.config.x.cookie_expiry)
     cookie_form.assign_attributes cookie_params.merge(seen: true)
@@ -10,16 +11,11 @@ class CookiesController < ApplicationController
   def create(cookie_expiry: Rails.application.config.x.cookie_expiry)
     cookie_form.assign_attributes cookie_params.merge(seen: true)
     cookies['cookie_setting'] = { value: cookie_form.to_json, expires: cookie_expiry }
-    redirect_to(return_path,
-                flash: { cookie_banner_confirmation: t("cookie_banner.confirmation_message.#{cookie_form.usage}") })
+    redirect_to path_only(params.dig(:cookie, :return_path)),
+                flash: { cookie_banner_confirmation: t("cookie_banner.confirmation_message.#{cookie_form.usage}") }
   end
 
   private
-
-  def return_path
-    uri = URI.parse(params.dig(:cookie, :return_path))
-    { only_path: true, path: uri.path, query: uri.query }
-  end
 
   def cookie_params
     params.require(:cookie).permit(:usage)
--- spec/controllers/cookies_controller_spec.rb
+++ spec/controllers/cookies_controller_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe CookiesController do
       expect(response).to render_template(:edit)
     end
   end
+
+  describe '#create' do
+    it 'redirects to the correct place' do
+      post :create, params: { cookie: { usage: true, return_path: '/test' } }
+      expect(response).to redirect_to('/test')
+    end
+  end
+
+  describe '#update' do
+    it 'redirects to the correct place' do
+      patch :update, params: { cookie: { usage: true } }
+      expect(response).to redirect_to('/about/cookies')
+    end
+  end
 end
